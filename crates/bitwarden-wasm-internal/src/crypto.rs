83d5fba6cd15af1a3459478c6e473846c5f11d48
Bernd Schoolmann <mail@quexten.com>
2024-11-01 12:59:41 +0100

    Add xchacha20poly1305

--- crates/bitwarden-wasm-internal/src/crypto.rs
+++ crates/bitwarden-wasm-internal/src/crypto.rs
@@ -1,9 +1,10 @@
-use std::rc::Rc;
+use std::{num::NonZero, rc::Rc};
 
 use bitwarden_core::{
     mobile::crypto::{InitOrgCryptoRequest, InitUserCryptoRequest},
     Client,
 };
+use bitwarden_crypto::chacha20;
 use wasm_bindgen::prelude::*;
 
 use crate::error::Result;
@@ -29,5 +30,18 @@ impl ClientCrypto {
     /// `initialize_user_crypto` but before any other crypto operations.
     pub async fn initialize_org_crypto(&self, req: InitOrgCryptoRequest) -> Result<()> {
         Ok(self.0.crypto().initialize_org_crypto(req).await?)
+    }
+
+    pub fn encrypt_xchacha20_poly1305(
+        secret_data: &[u8],
+        authenticated_data: &[u8],
+        key: &[u8],
+    ) -> Result<Vec<u8>> {
+        let key_len32 = key[..32].try_into().unwrap();
+        let (nonce, ciphertext) =
+            chacha20::encrypt_xchacha20_poly1305(secret_data, authenticated_data, key_len32)?;
+
+        let result: Vec<u8> = nonce.into_iter().chain(ciphertext.into_iter()).collect();
+        Ok(result)
     }
 }
