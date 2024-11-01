f1cdc2ed291aa8c0d4bb18e170f142b4e6610619
Bernd Schoolmann <mail@quexten.com>
2024-11-01 13:18:52 +0100

    Add logging

--- crates/bitwarden-wasm-internal/src/crypto.rs
+++ crates/bitwarden-wasm-internal/src/crypto.rs
@@ -37,11 +37,16 @@ impl ClientCrypto {
         authenticated_data: &[u8],
         key: &[u8],
     ) -> Result<Vec<u8>> {
+        println!("encrypt_xchacha20_poly1305");
+        println!("secret_data {:?}", secret_data);
+        println!("authenticated_data {:?}", authenticated_data);
+        println!("key {:?}", key);
         let key_len32 = key[..32].try_into().unwrap();
         let (nonce, ciphertext) =
             chacha20::encrypt_xchacha20_poly1305(secret_data, authenticated_data, key_len32)?;
-
+        println!("encrytped");
         let result: Vec<u8> = nonce.into_iter().chain(ciphertext.into_iter()).collect();
+        println!("result {:?}", result);
         Ok(result)
     }
 }
