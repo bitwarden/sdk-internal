03dbebabe1a610dc656dd11232a4685ecbbe4c3b
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-11-05 14:42:30 +0100

    refactor: convert into metadata trait/struct

--- crates/bitwarden-error/src/lib.rs
+++ crates/bitwarden-error/src/lib.rs
@@ -1,7 +1,1 @@
-pub trait BitwardenError {
-    /// The name of the error.
-    fn variant(&self) -> &'static str;
-
-    /// A human-readable description of the error.
-    fn message(&self) -> &'static str;
-}
+pub mod metadata;
--- crates/bitwarden-error/tests/mod.rs
+++ crates/bitwarden-error/tests/mod.rs
@@ -1,4 +1,4 @@
-use bitwarden_error::BitwardenError;
+use bitwarden_error::metadata::{AsErrorMetadata, ErrorMetadata};
 
 #[test]
 fn variant_for_basic_enum() {
@@ -8,17 +8,22 @@ fn variant_for_basic_enum() {
         Baz,
     }
 
-    impl BitwardenError for SimpleError {
-        fn variant(&self) -> &'static str {
+    impl AsErrorMetadata for SimpleError {
+        fn as_metadata(&self) -> ErrorMetadata {
             match self {
-                SimpleError::Foo => "SimpleError::Foo",
-                SimpleError::Bar => "SimpleError::Bar",
-                SimpleError::Baz => "SimpleError::Baz",
+                SimpleError::Foo => ErrorMetadata {
+                    name: "SimpleError::Foo",
+                    message: "An error occurred in the Foo variant",
+                },
+                SimpleError::Bar => ErrorMetadata {
+                    name: "SimpleError::Bar",
+                    message: "An error occurred in the Bar variant",
+                },
+                SimpleError::Baz => ErrorMetadata {
+                    name: "SimpleError::Baz",
+                    message: "An error occurred in the Baz variant",
+                },
             }
-        }
-
-        fn message(&self) -> &'static str {
-            todo!()
         }
     }
 
@@ -26,7 +31,7 @@ fn variant_for_basic_enum() {
     let bar = SimpleError::Bar;
     let baz = SimpleError::Baz;
 
-    assert_eq!(foo.variant(), "SimpleError::Foo");
-    assert_eq!(bar.variant(), "SimpleError::Bar");
-    assert_eq!(baz.variant(), "SimpleError::Baz");
+    assert_eq!(foo.as_metadata().name, "SimpleError::Foo");
+    assert_eq!(bar.as_metadata().name, "SimpleError::Bar");
+    assert_eq!(baz.as_metadata().name, "SimpleError::Baz");
 }
