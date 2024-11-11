c2511b3badcc4af719bea9380ba13cb601122cd8
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-11-11 16:08:20 +0100

    refactor: remove old BitwardenError trait

--- crates/bitwarden-error-macro/src/flat/attribute.rs
+++ crates/bitwarden-error-macro/src/flat/attribute.rs
@@ -1,13 +1,9 @@
 use quote::quote;
 
 pub(crate) fn bitwarden_error_flat(input: &syn::DeriveInput) -> proc_macro::TokenStream {
-    let type_identifier = &input.ident;
-
     quote! {
         #[derive(FlatError)]
         #input
-
-        impl BitwardenError for #type_identifier {}
     }
     .into()
 }
--- crates/bitwarden-error/src/lib.rs
+++ crates/bitwarden-error/src/lib.rs
@@ -7,11 +7,8 @@ pub mod wasm;
 pub mod prelude {
     pub use crate::basic_error;
     pub use crate::flat_error::FlatError;
-    pub use crate::BitwardenError;
     pub use bitwarden_error_macro::*;
 
     #[cfg(feature = "wasm")]
     pub use {crate::wasm::JsError, wasm_bindgen::prelude::*};
 }
-
-pub trait BitwardenError: flat_error::FlatError + ToString {}
