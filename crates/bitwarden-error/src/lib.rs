6843e4a9cc3b32c0599d56580e1d347a61ff9cb5
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-11-12 13:09:56 +0100

    chore: clean up unusued file

--- crates/bitwarden-error/src/lib.rs
+++ crates/bitwarden-error/src/lib.rs
@@ -1,11 +1,9 @@
-pub mod basic_error;
 pub mod flat_error;
 
 #[cfg(feature = "wasm")]
 pub mod wasm;
 
 pub mod prelude {
-    pub use crate::basic_error;
     pub use crate::flat_error::FlatError;
     pub use bitwarden_error_macro::*;
 
