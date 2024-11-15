a3db63fa02a3d026dda4bc80b6554edb95132a41
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-11-15 10:06:41 +0100

    refactor: reduce rename duplication

--- crates/bitwarden-error-macro/src/args.rs
+++ crates/bitwarden-error-macro/src/args.rs
@@ -10,16 +10,14 @@ pub(crate) struct BitwardenErrorArgs {
 }
 
 #[derive(FromMeta)]
+#[darling(rename_all = "snake_case")]
 pub(crate) enum BitwardenErrorType {
     /// The error is going to be converted into a string using the `ToString` trait
-    #[darling(rename = "basic")]
     Basic,
 
     /// The error is going to be converted into a flat error using the `FlatError` trait
-    #[darling(rename = "flat")]
     Flat,
 
     /// The entire error stack is going to be made available using `serde`
-    #[darling(rename = "full")]
     Full,
 }
