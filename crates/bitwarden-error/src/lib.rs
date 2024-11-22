4c822419c16a4accb29717e2bdde6e04eb360757
Hinton <hinton@users.noreply.github.com>
2024-11-22 09:30:24 +0100

    Remove need to depend on js-sys when consuming errors

--- crates/bitwarden-error/src/lib.rs
+++ crates/bitwarden-error/src/lib.rs
@@ -3,6 +3,10 @@ pub mod flat_error;
 #[cfg(feature = "wasm")]
 pub mod wasm;
 
+/// Re-export the `js_sys` crate since the proc macro depends on it.
+#[cfg(feature = "wasm")]
+pub use ::js_sys;
+
 pub mod prelude {
     pub use bitwarden_error_macro::*;
 
