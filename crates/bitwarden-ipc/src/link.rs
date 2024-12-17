58d48e58146d2dcce10e8bdeb130f0a383fdc788
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-12-17 09:40:08 +0100

    fix: desugar async in Link

--- crates/bitwarden-ipc/src/link.rs
+++ crates/bitwarden-ipc/src/link.rs
@@ -4,8 +4,8 @@ use crate::destination::Destination;
 /// This is a low-level struct that is used to send and receive data between two endpoints
 /// it is not meant to be used directly by consumers of this library.
 pub trait Link {
-    async fn send(&self, data: &[u8]);
-    async fn receive(&self) -> Vec<u8>;
+    fn send(&self, data: &[u8]) -> impl std::future::Future<Output = ()>;
+    fn receive(&self) -> impl std::future::Future<Output = Vec<u8>>;
 
     fn available_destinations(&self) -> Vec<Destination>;
 }
