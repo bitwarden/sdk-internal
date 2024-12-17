56c5a7fa05b4fb2029fb5cca3e152548d22973ec
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-12-17 09:26:27 +0100

    feat: make Link async

--- crates/bitwarden-ipc/src/link.rs
+++ crates/bitwarden-ipc/src/link.rs
@@ -4,8 +4,8 @@ use crate::destination::Destination;
 /// This is a low-level struct that is used to send and receive data between two endpoints
 /// it is not meant to be used directly by consumers of this library.
 pub trait Link {
-    fn send(&self, data: &[u8]);
-    fn receive(&self) -> Vec<u8>;
+    async fn send(&self, data: &[u8]);
+    async fn receive(&self) -> Vec<u8>;
 
     fn available_destinations(&self) -> Vec<Destination>;
 }
--- crates/bitwarden-ipc/src/wasm/link.rs
+++ crates/bitwarden-ipc/src/wasm/link.rs
@@ -1,3 +1,4 @@
+use js_sys::Uint8Array;
 use wasm_bindgen::prelude::*;
 
 use crate::destination::Destination;
@@ -6,8 +7,8 @@ use crate::link::Link;
 #[wasm_bindgen(typescript_custom_section)]
 const ITEXT_STYLE: &'static str = r#"
 export interface Link {
-    send(data: Uint8Array): void;
-    receive(): Uint8Array;
+    send(data: Uint8Array): Promise<void>;
+    receive(): Promise<Uint8Array>;
     availableDestinations(): Destination[];
 }
 "#;
@@ -18,22 +19,24 @@ extern "C" {
     pub type JsLink;
 
     #[wasm_bindgen(method, structural)]
-    pub fn send(this: &JsLink, data: &[u8]);
+    pub async fn send(this: &JsLink, data: &[u8]);
 
     #[wasm_bindgen(method, structural)]
-    pub fn receive(this: &JsLink) -> Vec<u8>;
+    pub async fn receive(this: &JsLink) -> JsValue;
 
     #[wasm_bindgen(method, structural)]
     pub fn availableDestinations(this: &JsLink) -> Vec<Destination>;
 }
 
 impl Link for JsLink {
-    fn send(&self, data: &[u8]) {
-        self.send(data);
+    async fn send(&self, data: &[u8]) {
+        self.send(data).await;
     }
 
-    fn receive(&self) -> Vec<u8> {
-        self.receive()
+    async fn receive(&self) -> Vec<u8> {
+        let js_value = self.receive().await;
+        let array = Uint8Array::new(&js_value);
+        array.to_vec()
     }
 
     fn available_destinations(&self) -> Vec<Destination> {
