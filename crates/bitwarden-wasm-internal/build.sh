cd5fa2764d1e21c0c3c1bc652acb6de130966e4f
Bernd Schoolmann <mail@quexten.com>
2025-01-16 14:45:46 +0100

    Fix wasm build

--- crates/bitwarden-wasm-internal/build.sh
+++ crates/bitwarden-wasm-internal/build.sh
@@ -11,13 +11,12 @@ git rev-parse HEAD > ./crates/bitwarden-wasm-internal/npm/VERSION
 
 if [ "$1" != "-r" ]; then
   # Dev
-  cargo build -p bitwarden-wasm-internal --target wasm32-unknown-unknown
+  cargo build -p bitwarden-wasm-internal --target wasm32-unknown-unknown --release --config 'patch.crates-io.pkcs5.git="https://github.com/bitwarden/rustcrypto-formats.git"'  --config 'patch.crates-io.pkcs5.rev="2b27c63034217dd126bbf5ed874da51b84f8c705"'
   wasm-bindgen --target bundler --out-dir crates/bitwarden-wasm-internal/npm ./target/wasm32-unknown-unknown/debug/bitwarden_wasm_internal.wasm
   wasm-bindgen --target nodejs --out-dir crates/bitwarden-wasm-internal/npm/node ./target/wasm32-unknown-unknown/debug/bitwarden_wasm_internal.wasm
 else
   # Release
-  cargo build -p bitwarden-wasm-internal --target wasm32-unknown-unknown --release
-  wasm-bindgen --target bundler --out-dir crates/bitwarden-wasm-internal/npm ./target/wasm32-unknown-unknown/release/bitwarden_wasm_internal.wasm
+  cargo build -p bitwarden-wasm-internal --target wasm32-unknown-unknown --release --config 'patch.crates-io.pkcs5.git="https://github.com/bitwarden/rustcrypto-formats.git"'  --config 'patch.crates-io.pkcs5.rev="2b27c63034217dd126bbf5ed874da51b84f8c705"'
   wasm-bindgen --target nodejs --out-dir crates/bitwarden-wasm-internal/npm/node ./target/wasm32-unknown-unknown/release/bitwarden_wasm_internal.wasm
 fi
 
