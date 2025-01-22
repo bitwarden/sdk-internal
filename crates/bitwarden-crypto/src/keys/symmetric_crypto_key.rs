122ed7d3c0e70db1e99335e7bc2bdedbb9739950
Bernd Schoolmann <mail@quexten.com>
2025-01-22 13:39:17 +0100

    Cargo fmt

--- crates/bitwarden-crypto/src/keys/symmetric_crypto_key.rs
+++ crates/bitwarden-crypto/src/keys/symmetric_crypto_key.rs
@@ -7,10 +7,9 @@ use rand::Rng;
 use zeroize::Zeroize;
 
 use super::key_encryptable::CryptoKey;
-use crate::CryptoError;
-
 #[cfg(not(test))]
 use super::master_key::KdfDerivedKeymaterial;
+use crate::CryptoError;
 
 // GenericArray is equivalent to [u8; N], which is a Copy type placed on the stack.
 // To keep the compiler from making stack copies when moving this struct around,
