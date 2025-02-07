a899c1a71bbd1711919907f8b90dcd8a965cb046
Bernd Schoolmann <mail@quexten.com>
2025-02-07 18:53:17 +0100

    Clippy errors

--- crates/bitwarden-crypto/src/keys/key_hash.rs
+++ crates/bitwarden-crypto/src/keys/key_hash.rs
@@ -1,4 +1,3 @@
-use base64::{engine::general_purpose::STANDARD, Engine};
 use serde::{Deserialize, Serialize};
 
 #[derive(PartialEq, Serialize, Deserialize, Debug, Clone)]
@@ -6,28 +5,10 @@ pub(crate) enum KeyHashAlgorithm {
     Blake3,
 }
 
-impl KeyHashAlgorithm {
-    pub(crate) fn to_string(&self) -> String {
-        match self {
-            Self::Blake3 => "blake3".to_string(),
-        }
-    }
-}
-
 #[derive(PartialEq, Serialize, Deserialize, Debug, Clone)]
 pub(crate) struct KeyHash {
     pub(crate) hash: Vec<u8>,
     pub(crate) algorithm: KeyHashAlgorithm,
-}
-
-impl KeyHash {
-    pub(crate) fn to_string(&self) -> String {
-        format!(
-            "{}:{}",
-            self.algorithm.to_string(),
-            STANDARD.encode(&self.hash)
-        )
-    }
 }
 
 pub(crate) trait KeyHashable {
