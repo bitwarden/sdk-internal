674d972470ecc934e3fa95ff185d27b456dc53b1
Bernd Schoolmann <mail@quexten.com>
2025-02-13 15:11:48 +0100

    Cargo fmt

--- crates/bitwarden-vault/src/cipher/ssh_key.rs
+++ crates/bitwarden-vault/src/cipher/ssh_key.rs
@@ -2,7 +2,6 @@ use bitwarden_core::key_management::{KeyIds, SymmetricKeyId};
 use bitwarden_crypto::{CryptoError, Decryptable, EncString, Encryptable, KeyStoreContext};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-
 #[cfg(feature = "wasm")]
 use tsify_next::Tsify;
 
