31a8c7fa7055bcbb7baf23f56e23c7eb7d5e8054
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-02-17 10:57:42 +0100

    feat: add error types to session repository

--- crates/bitwarden-ipc/src/traits/session_repository.rs
+++ crates/bitwarden-ipc/src/traits/session_repository.rs
@@ -4,20 +4,25 @@ use tokio::sync::RwLock;
 
 use crate::endpoint::Endpoint;
 
-// TODO: Might want to allow the operations to fail with an error
 pub trait SessionRepository {
     type Session;
+    type GetError;
+    type SaveError;
+    type RemoveError;
 
     fn get(
         &self,
         destination: Endpoint,
-    ) -> impl std::future::Future<Output = Option<Self::Session>>;
+    ) -> impl std::future::Future<Output = Result<Option<Self::Session>, Self::GetError>>;
     fn save(
         &self,
         destination: Endpoint,
         session: Self::Session,
-    ) -> impl std::future::Future<Output = ()>;
-    fn remove(&self, destination: Endpoint) -> impl std::future::Future<Output = ()>;
+    ) -> impl std::future::Future<Output = Result<(), Self::SaveError>>;
+    fn remove(
+        &self,
+        destination: Endpoint,
+    ) -> impl std::future::Future<Output = Result<(), Self::RemoveError>>;
 }
 
 pub type InMemorySessionRepository<Session> = RwLock<HashMap<Endpoint, Session>>;
@@ -26,16 +31,21 @@ where
     Session: Clone,
 {
     type Session = Session;
+    type GetError = ();
+    type SaveError = ();
+    type RemoveError = ();
 
-    async fn get(&self, destination: Endpoint) -> Option<Self::Session> {
-        self.read().await.get(&destination).cloned()
+    async fn get(&self, destination: Endpoint) -> Result<Option<Self::Session>, ()> {
+        Ok(self.read().await.get(&destination).cloned())
     }
 
-    async fn save(&self, destination: Endpoint, session: Self::Session) {
+    async fn save(&self, destination: Endpoint, session: Self::Session) -> Result<(), ()> {
         self.write().await.insert(destination, session);
+        Ok(())
     }
 
-    async fn remove(&self, destination: Endpoint) {
+    async fn remove(&self, destination: Endpoint) -> Result<(), ()> {
         self.write().await.remove(&destination);
+        Ok(())
     }
 }
