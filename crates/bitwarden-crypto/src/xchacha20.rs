6b241ece78bb39fb13623a72109ca1712372a82f
Bernd Schoolmann <mail@quexten.com>
2025-03-07 17:17:39 +0100

    Adjust interface according to feedback

--- crates/bitwarden-crypto/src/xchacha20.rs
+++ crates/bitwarden-crypto/src/xchacha20.rs
@@ -16,32 +16,44 @@
 //!
 
 use chacha20poly1305::{AeadCore, AeadInPlace, KeyInit, XChaCha20Poly1305};
-use generic_array::{typenum::U24, GenericArray};
+use generic_array::GenericArray;
+use rand::{CryptoRng, RngCore};
 
 use crate::CryptoError;
 
 #[allow(unused)]
-pub(crate) fn generate_nonce() -> GenericArray<u8, U24> {
-    XChaCha20Poly1305::generate_nonce(rand::thread_rng())
+pub(crate) struct XChaCha20Poly1305Ciphertext {
+    nonce: GenericArray<u8, <XChaCha20Poly1305 as AeadCore>::NonceSize>,
+    ciphertext: Vec<u8>,
 }
 
 #[allow(unused)]
-pub(crate) fn encrypt_xchacha20_poly1305(
-    nonce: &[u8; 24],
+fn encrypt_xchacha20_poly1305(
     key: &[u8; 32],
     plaintext_secret_data: &[u8],
     associated_data: &[u8],
-) -> Result<Vec<u8>, CryptoError> {
+) -> XChaCha20Poly1305Ciphertext {
+    let mut rng = rand::thread_rng();
+    encrypt_xchacha20_poly1305_internal(rng, key, plaintext_secret_data, associated_data)
+}
+
+fn encrypt_xchacha20_poly1305_internal(
+    rng: impl RngCore + CryptoRng,
+    key: &[u8; 32],
+    plaintext_secret_data: &[u8],
+    associated_data: &[u8],
+) -> XChaCha20Poly1305Ciphertext {
+    let nonce = &XChaCha20Poly1305::generate_nonce(rng);
     // This buffer contains the plaintext, that will be encrypted in-place
-    let mut buffer = Vec::from(plaintext_secret_data);
+    let mut buffer = plaintext_secret_data.to_vec();
     XChaCha20Poly1305::new(GenericArray::from_slice(key))
-        .encrypt_in_place(
-            GenericArray::from_slice(nonce),
-            associated_data,
-            &mut buffer,
-        )
-        .map_err(|_| CryptoError::InvalidKey)?;
-    Ok(buffer)
+        .encrypt_in_place(&nonce, associated_data, &mut buffer)
+        .expect("encryption failed");
+
+    XChaCha20Poly1305Ciphertext {
+        nonce: *nonce,
+        ciphertext: buffer,
+    }
 }
 
 #[allow(unused)]
@@ -58,7 +70,7 @@ pub(crate) fn decrypt_xchacha20_poly1305(
             associated_data,
             &mut buffer,
         )
-        .map_err(|_| CryptoError::InvalidKey)?;
+        .map_err(|_| CryptoError::KeyDecrypt)?;
     Ok(buffer)
 }
 
@@ -71,13 +83,14 @@ mod tests {
         let key = [0u8; 32];
         let plaintext_secret_data = b"My secret data";
         let authenticated_data = b"My authenticated data";
-        let nonce = generate_nonce().into();
-
-        let encrypted =
-            encrypt_xchacha20_poly1305(&nonce, &key, plaintext_secret_data, authenticated_data)
-                .unwrap();
-        let decrypted =
-            decrypt_xchacha20_poly1305(&nonce, &key, &encrypted, authenticated_data).unwrap();
+        let encrypted = encrypt_xchacha20_poly1305(&key, plaintext_secret_data, authenticated_data);
+        let decrypted = decrypt_xchacha20_poly1305(
+            &encrypted.nonce.into(),
+            &key,
+            &encrypted.ciphertext,
+            authenticated_data,
+        )
+        .unwrap();
         assert_eq!(plaintext_secret_data, decrypted.as_slice());
     }
 
@@ -86,13 +99,16 @@ mod tests {
         let key = [0u8; 32];
         let plaintext_secret_data = b"My secret data";
         let authenticated_data = b"My authenticated data";
-        let nonce = generate_nonce().into();
 
         let mut encrypted =
-            encrypt_xchacha20_poly1305(&nonce, &key, plaintext_secret_data, authenticated_data)
-                .unwrap();
-        encrypted[0] = encrypted[0].wrapping_add(1);
-        let result = decrypt_xchacha20_poly1305(&nonce, &key, &encrypted, authenticated_data);
+            encrypt_xchacha20_poly1305(&key, plaintext_secret_data, authenticated_data);
+        encrypted.ciphertext[0] = encrypted.ciphertext[0].wrapping_add(1);
+        let result = decrypt_xchacha20_poly1305(
+            &encrypted.nonce.into(),
+            &key,
+            &encrypted.ciphertext,
+            authenticated_data,
+        );
         assert!(result.is_err());
     }
 
@@ -101,20 +117,14 @@ mod tests {
         let key = [0u8; 32];
         let plaintext_secret_data = b"My secret data";
         let mut authenticated_data = b"My authenticated data".to_vec();
-        let nonce = generate_nonce().into();
 
-        let encrypted = encrypt_xchacha20_poly1305(
-            &nonce,
-            &key,
-            plaintext_secret_data,
-            authenticated_data.as_slice(),
-        )
-        .unwrap();
+        let encrypted =
+            encrypt_xchacha20_poly1305(&key, plaintext_secret_data, authenticated_data.as_slice());
         authenticated_data[0] = authenticated_data[0].wrapping_add(1);
         let result = decrypt_xchacha20_poly1305(
-            &nonce,
+            &encrypted.nonce.into(),
             &key,
-            encrypted.as_slice(),
+            &encrypted.ciphertext,
             authenticated_data.as_slice(),
         );
         assert!(result.is_err());
@@ -125,13 +135,16 @@ mod tests {
         let key = [0u8; 32];
         let plaintext_secret_data = b"My secret data";
         let authenticated_data = b"My authenticated data";
-        let mut nonce = generate_nonce().into();
 
-        let encrypted =
-            encrypt_xchacha20_poly1305(&nonce, &key, plaintext_secret_data, authenticated_data)
-                .unwrap();
-        nonce[0] = nonce[0].wrapping_add(1);
-        let result = decrypt_xchacha20_poly1305(&nonce, &key, &encrypted, authenticated_data);
+        let mut encrypted =
+            encrypt_xchacha20_poly1305(&key, plaintext_secret_data, authenticated_data);
+        encrypted.nonce[0] = encrypted.nonce[0].wrapping_add(1);
+        let result = decrypt_xchacha20_poly1305(
+            &encrypted.nonce.into(),
+            &key,
+            &encrypted.ciphertext,
+            authenticated_data,
+        );
         assert!(result.is_err());
     }
 }
