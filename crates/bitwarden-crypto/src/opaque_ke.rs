27f94e1847e189e3eae868d62787afc69352da88
Bernd Schoolmann <mail@quexten.com>
2025-03-11 17:16:40 +0100

    Fix clippy

--- crates/bitwarden-crypto/src/opaque_ke.rs
+++ crates/bitwarden-crypto/src/opaque_ke.rs
@@ -76,7 +76,8 @@ impl Ksf for Argon2Id {
         let res = argon2::Argon2::new(
             argon2::Algorithm::Argon2id,
             argon2::Version::V0x13,
-            Params::new(self.m_cost, self.t_cost, self.p_cost, Some(32)).map_err(|_| InternalError::KsfError)?,
+            Params::new(self.m_cost, self.t_cost, self.p_cost, Some(32))
+                .map_err(|_| InternalError::KsfError)?,
         );
         res.hash_password_into(&input, &[0; argon2::RECOMMENDED_SALT_LEN], &mut output)
             .map_err(|_| InternalError::KsfError)?;
