83a7eb123baa88581db352f431dee7bd3bd49163
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-03-14 14:14:02 +0100

    feat: use named id field

--- crates/bitwarden-ipc/src/endpoint.rs
+++ crates/bitwarden-ipc/src/endpoint.rs
@@ -5,7 +5,7 @@ use {tsify_next::Tsify, wasm_bindgen::prelude::*};
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "wasm", derive(Tsify), tsify(into_wasm_abi, from_wasm_abi))]
 pub enum Endpoint {
-    Web(i32),
+    Web { id: i32 },
     BrowserForeground,
     BrowserBackground,
     DesktopRenderer,
