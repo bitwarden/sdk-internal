b075993aaee8ec02a5ba12d091685a1337ef8e97
Bernd Schoolmann <mail@quexten.com>
2025-03-27 15:37:27 +0100

    Cargo fmt

--- crates/bitwarden-crypto/src/enc_string/asymmetric.rs
+++ crates/bitwarden-crypto/src/enc_string/asymmetric.rs
@@ -149,8 +149,9 @@ impl serde::Serialize for AsymmetricEncString {
 }
 
 impl AsymmetricEncString {
-    /// Encapsulate a symmetric key, to be shared asymmetrically. Produces a [AsymmetricEncString::Rsa2048_OaepSha1_B64] variant.
-    /// Note, this does not sign the data and thus does not guarantee sender authenticity.
+    /// Encapsulate a symmetric key, to be shared asymmetrically. Produces a
+    /// [AsymmetricEncString::Rsa2048_OaepSha1_B64] variant. Note, this does not sign the data
+    /// and thus does not guarantee sender authenticity.
     pub fn encapsulate_key_unsigned(
         encapsulated_key: &SymmetricCryptoKey,
         encapsulation_key: &dyn AsymmetricEncryptable,
@@ -176,8 +177,9 @@ impl AsymmetricEncString {
 }
 
 impl AsymmetricEncString {
-    /// Decapsulate a symmetric key, shared asymmetrically. 
-    /// Note: The shared key does not have a sender signature and sender authenticity is not guaranteed.
+    /// Decapsulate a symmetric key, shared asymmetrically.
+    /// Note: The shared key does not have a sender signature and sender authenticity is not
+    /// guaranteed.
     pub fn decapsulate_key_unsigned(
         &self,
         decapsulation_key: &AsymmetricCryptoKey,
