c55822f7e42f9b004ce40191b9df7f85bdcf555a
gbubemismith <gsmithwalter@gmail.com>
2025-03-27 17:39:42 -0400

    exposed decrypt fucntionality through wasm

--- crates/bitwarden-wasm-internal/src/vault/ciphers.rs
+++ crates/bitwarden-wasm-internal/src/vault/ciphers.rs
@@ -1,7 +1,10 @@
 use std::rc::Rc;
 
 use bitwarden_core::Client;
-use bitwarden_vault::{Cipher, CipherView, DecryptError, EncryptError, VaultClientExt};
+use bitwarden_vault::{
+    Cipher, CipherListView, CipherView, DecryptError, EncryptError, Fido2CredentialView,
+    VaultClientExt,
+};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
@@ -37,5 +40,35 @@ impl ClientCiphers {
     /// - `Err(DecryptError)` if decryption fails
     pub fn decrypt(&self, cipher: Cipher) -> Result<CipherView, DecryptError> {
         self.0.vault().ciphers().decrypt(cipher)
+    }
+
+    /// Decrypt list of ciphers
+    ///
+    /// # Arguments
+    /// - `ciphers` - The list of encrypted ciphers to decrypt
+    ///
+    /// # Returns
+    /// - `Ok(Vec<CipherListView>)` containing the decrypted ciphers
+    /// - `Err(DecryptError)` if decryption fails
+    pub fn decrypt_list(&self, ciphers: Vec<Cipher>) -> Result<Vec<CipherListView>, DecryptError> {
+        self.0.vault().ciphers().decrypt_list(ciphers)
+    }
+
+    /// Decrypt FIDO2 credentials
+    ///
+    /// # Arguments
+    /// - `cipher_view` - Cipher to encrypt containing the FIDO2 credential
+    ///
+    /// # Returns
+    /// - `Ok(Vec<Fido2CredentialView>)` containing the decrypted FIDO2 credentials
+    /// - `Err(DecryptError)` if decryption fails
+    pub fn decrypt_fido2_credentials(
+        &self,
+        cipher_view: CipherView,
+    ) -> Result<Vec<Fido2CredentialView>, DecryptError> {
+        self.0
+            .vault()
+            .ciphers()
+            .decrypt_fido2_credentials(cipher_view)
     }
 }
