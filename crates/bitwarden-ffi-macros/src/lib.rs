805d3f86d7a0071eed15d8997944b1e2251fd18c
Daniel García <dani-garcia@users.noreply.github.com>
2025-04-16 18:41:33 +0200

    Refactor

--- crates/bitwarden-ffi-macros/src/lib.rs
+++ crates/bitwarden-ffi-macros/src/lib.rs
@@ -7,8 +7,8 @@ pub fn extern_wasm_channel(
     args: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    match extern_wasm_channel::extern_wasm_channel_internal(args, item) {
-        Ok(v) => v,
+    match extern_wasm_channel::extern_wasm_channel_internal(args.into(), item.into()) {
+        Ok(v) => v.into(),
         Err(e) => proc_macro::TokenStream::from(e.to_compile_error()),
     }
 }
