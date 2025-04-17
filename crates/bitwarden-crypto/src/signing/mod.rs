a80c102e89f8c1979c1c91ee2902d7d5567fa61f
Bernd Schoolmann <mail@quexten.com>
2025-04-17 19:21:23 +0200

    Simplify comment

--- crates/bitwarden-crypto/src/signing/mod.rs
+++ crates/bitwarden-crypto/src/signing/mod.rs
@@ -1,7 +1,6 @@
 /// Signing is domain-separated within bitwarden, to prevent cross protocol attacks.
 ///
-/// A new signed entity or protocol shall use a new signing namespace. Further, signing
-/// namespaces cannot be renamed, since that would invalidate signatures.
+/// A new signed entity or protocol shall use a new signing namespace. 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum SigningNamespace {
     #[allow(dead_code)]
