b2f3044695c35d978726c8fbb6be7fcd5a224f99
Bernd Schoolmann <mail@quexten.com>
2025-04-21 12:28:47 +0200

    Merge changes

--- crates/bitwarden-crypto/src/enc_string/symmetric.rs
+++ crates/bitwarden-crypto/src/enc_string/symmetric.rs
@@ -253,7 +253,7 @@ impl EncString {
             .create_ciphertext(data_dec, &[], |data, aad| {
                 let ciphertext =
                     crate::xchacha20::encrypt_xchacha20_poly1305(&(*key.enc_key).into(), data, aad);
-                nonce.copy_from_slice(ciphertext.nonce().as_slice());
+                nonce = *ciphertext.nonce();
                 ciphertext.encrypted_bytes()
             })
             .unprotected(coset::HeaderBuilder::new().iv(nonce.to_vec()).build())
@@ -372,7 +372,7 @@ mod tests {
         let enc_key = [0u8; 32];
         let key = SymmetricCryptoKey::XChaCha20Poly1305Key(crate::XChaCha20Poly1305Key {
             key_id,
-            enc_key: Box::pin(*GenericArray::from_slice(enc_key.as_slice())),
+            enc_key: Box::pin(enc_key.into()),
         });
 
         let test_string = "encrypted_test_string";
