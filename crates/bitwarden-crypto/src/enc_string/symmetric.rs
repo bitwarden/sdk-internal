0ff0e989abe56d193cce201df6c5d1ed8fea4bd7
Bernd Schoolmann <accounts@quexten.com>
2025-04-21 12:06:29 +0200

    Update crates/bitwarden-crypto/src/enc_string/symmetric.rs
    Co-authored-by: Daniel García <dani-garcia@users.noreply.github.com>

--- crates/bitwarden-crypto/src/enc_string/symmetric.rs
+++ crates/bitwarden-crypto/src/enc_string/symmetric.rs
@@ -369,7 +369,7 @@ mod tests {
         let enc_key = [0u8; 32];
         let key = SymmetricCryptoKey::XChaCha20Poly1305Key(crate::XChaCha20Poly1305Key {
             key_id,
-            enc_key: Box::pin(*GenericArray::from_slice(enc_key.as_slice())),
+            enc_key: Box::pin(enc_key.into()),
         });
 
         let test_string = "encrypted_test_string";
