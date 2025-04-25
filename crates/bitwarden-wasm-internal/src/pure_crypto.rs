252a097b1ed6feb7c7cb50eab04cd4f455a61755
Bernd Schoolmann <mail@quexten.com>
2025-04-25 12:46:40 +0200

    Add tests

--- crates/bitwarden-wasm-internal/src/pure_crypto.rs
+++ crates/bitwarden-wasm-internal/src/pure_crypto.rs
@@ -22,7 +22,7 @@ impl PureCrypto {
         EncString::from_str(&enc_string)?.decrypt_with_key(&SymmetricCryptoKey::try_from(key)?)
     }
 
-    pub fn symmetric_decrypt_to_bytes(
+    pub fn symmetric_decrypt_bytes(
         enc_string: String,
         key: Vec<u8>,
     ) -> Result<Vec<u8>, CryptoError> {
@@ -102,7 +102,7 @@ impl PureCrypto {
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
+    use std::{num::NonZero, str::FromStr};
 
     use bitwarden_crypto::EncString;
 
@@ -142,7 +142,7 @@ mod tests {
     }
 
     #[test]
-    fn test_symmetric_round_trip() {
+    fn test_symmetric_string_round_trip() {
         let encrypted =
             PureCrypto::symmetric_encrypt_string(DECRYPTED.to_string(), KEY.to_vec()).unwrap();
         let decrypted =
@@ -151,8 +151,17 @@ mod tests {
     }
 
     #[test]
+    fn test_symmetric_bytes_round_trip() {
+        let encrypted =
+            PureCrypto::symmetric_encrypt_bytes(DECRYPTED.as_bytes().to_vec(), KEY.to_vec()).unwrap();
+        let decrypted =
+            PureCrypto::symmetric_decrypt_bytes(encrypted.clone(), KEY.to_vec()).unwrap();
+        assert_eq!(decrypted, DECRYPTED.as_bytes().to_vec());
+    }
+
+    #[test]
     fn test_symmetric_decrypt_array_buffer() {
-        let result = PureCrypto::symmetric_encrypt_filedata(ENCRYPTED_BYTES.to_vec(), KEY.to_vec());
+        let result = PureCrypto::symmetric_decrypt_filedata(ENCRYPTED_BYTES.to_vec(), KEY.to_vec());
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), DECRYPTED_BYTES);
     }
@@ -165,11 +174,46 @@ mod tests {
     }
 
     #[test]
-    fn test_symmetric_buffer_round_trip() {
+    fn test_symmetric_filedata_round_trip() {
         let encrypted =
             PureCrypto::symmetric_encrypt_filedata(DECRYPTED_BYTES.to_vec(), KEY.to_vec()).unwrap();
         let decrypted =
-            PureCrypto::symmetric_encrypt_filedata(encrypted.clone(), KEY.to_vec()).unwrap();
+            PureCrypto::symmetric_decrypt_filedata(encrypted.clone(), KEY.to_vec()).unwrap();
         assert_eq!(decrypted, DECRYPTED_BYTES);
+    }
+
+    #[test]
+    fn test_generate_aes256_cbc_hmac() {
+        let key = PureCrypto::generate_user_key_aes256_cbc_hmac();
+        assert_eq!(key.len(), 64);
+    }
+
+    #[test]
+    fn test_generate_xchacha20_poly1305() {
+        let key = PureCrypto::generate_user_key_xchacha20_poly1305();
+        assert!(key.len() > 64);
+    }
+
+    #[test]
+    fn roundtrip_encrypt_user_key_with_master_password() {
+        let master_password = "test";
+        let email = "test@example.com";
+        let kdf = Kdf::PBKDF2 { iterations: NonZero::try_from(600000).unwrap() };
+        let user_key = PureCrypto::generate_user_key_aes256_cbc_hmac();
+        let encrypted_user_key = PureCrypto::encrypt_user_key_with_master_password(
+            user_key.clone(),
+            master_password.to_string(),
+            email.to_string(),
+            kdf.clone(),
+        )
+        .unwrap();
+        let decrypted_user_key = PureCrypto::decrypt_user_key_with_master_password(
+            encrypted_user_key,
+            master_password.to_string(),
+            email.to_string(),
+            kdf,
+        )
+        .unwrap();
+        assert_eq!(user_key, decrypted_user_key);
     }
 }
