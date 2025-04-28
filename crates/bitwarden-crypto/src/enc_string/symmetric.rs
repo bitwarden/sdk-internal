6214a50cc66be04a8d5fd534817f1f600b6c3e79
Bernd Schoolmann <mail@quexten.com>
2025-04-28 13:39:45 +0200

    Cleanup nonce in XChaChaPoly1305Ciphertext

--- crates/bitwarden-crypto/src/enc_string/symmetric.rs
+++ crates/bitwarden-crypto/src/enc_string/symmetric.rs
@@ -272,7 +272,7 @@ impl EncString {
             .create_ciphertext(data_dec, &[], |data, aad| {
                 let ciphertext =
                     crate::xchacha20::encrypt_xchacha20_poly1305(&(*key.enc_key).into(), data, aad);
-                nonce = *ciphertext.nonce();
+                nonce = ciphertext.nonce();
                 ciphertext.encrypted_bytes()
             })
             .unprotected(coset::HeaderBuilder::new().iv(nonce.to_vec()).build())
