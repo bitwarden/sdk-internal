b49153bca86851437d672701acd53cf0ab22b7a9
Bernd Schoolmann <mail@quexten.com>
2025-05-01 15:22:21 +0200

    Change key ID to use uuid v4

--- crates/bitwarden-crypto/src/keys/key_id.rs
+++ crates/bitwarden-crypto/src/keys/key_id.rs
@@ -1,8 +1,13 @@
 use rand::RngCore;
+use uuid::Uuid;
 
+/// Since `KeyId` is a wrapper around UUIDs, this is statically 16 bytes.
 pub(crate) const KEY_ID_SIZE: usize = 16;
 
-pub(crate) struct KeyId([u8; KEY_ID_SIZE]);
+/// A key id is a unique identifier for a single key. There is a 1:1 mapping between key ID and key
+/// bytes, so something like a user key rotation is replacing the key with ID A with a new key with
+/// ID B.
+pub(crate) struct KeyId(uuid::Uuid);
 
 /// Fixed length identifiers for keys.
 /// These are intended to be unique and constant per-key.
@@ -11,18 +16,34 @@ pub(crate) struct KeyId([u8; KEY_ID_SIZE]);
 /// generate with vanishingly small collision chance. However, the generation of IDs is an internal
 /// concern and may change in the future.
 impl KeyId {
-    pub fn as_bytes(&self) -> &[u8; KEY_ID_SIZE] {
-        &self.0
-    }
+    /// Creates a new random key ID randomly, sampled from the crates CSPRNG.
+    pub fn make() -> Self {
+        // We do not use the uuid crate's random generation functionality here to make sure the
+        // entropy sampling aligns with the rest of this crates usage of CSPRNGs.
+        let mut random_seed = [0u8; 16];
+        rand::thread_rng().fill_bytes(&mut random_seed);
 
-    pub fn from_bytes(bytes: [u8; KEY_ID_SIZE]) -> Self {
-        KeyId(bytes)
+        let uuid = uuid::Builder::from_random_bytes(random_seed)
+            .with_version(uuid::Version::Random)
+            .with_variant(uuid::Variant::RFC4122);
+        Self(uuid.into_uuid())
     }
+}
 
-    pub fn generate() -> Self {
-        let mut rng = rand::thread_rng();
-        let mut key_id = [0u8; KEY_ID_SIZE];
-        rng.fill_bytes(&mut key_id);
-        Self::from_bytes(key_id)
+impl Into<[u8; KEY_ID_SIZE]> for KeyId {
+    fn into(self) -> [u8; KEY_ID_SIZE] {
+        self.0.as_bytes().clone()
+    }
+}
+
+impl From<[u8; KEY_ID_SIZE]> for KeyId {
+    fn from(bytes: [u8; KEY_ID_SIZE]) -> Self {
+        KeyId(Uuid::from_bytes(bytes))
+    }
+}
+
+impl std::fmt::Debug for KeyId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "KeyId({})", self.0)
     }
 }
