64c1dbe354a627267ed4946cbd64420147462c34
Bernd Schoolmann <accounts@quexten.com>
2025-05-05 10:37:17 +0200

    Update crates/bitwarden-crypto/src/cose.rs
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- crates/bitwarden-crypto/src/cose.rs
+++ crates/bitwarden-crypto/src/cose.rs
@@ -90,6 +90,7 @@ impl TryFrom<&coset::CoseKey> for SymmetricCryptoKey {
 
         match alg {
             coset::Algorithm::PrivateUse(XCHACHA20_POLY1305) => {
+                // Ensure the length is correct since `GenericArray::clone_from_slice` panics if it receives the wrong length.
                 if key_bytes.len() != xchacha20::KEY_SIZE {
                     return Err(CryptoError::InvalidKey);
                 }
