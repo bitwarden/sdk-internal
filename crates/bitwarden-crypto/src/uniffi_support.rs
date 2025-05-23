0dcbb77982c3231db7c4352423da76414c4f9f16
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-05-23 11:16:16 +0200

    feat: partial upgrade the rest is blocked

--- crates/bitwarden-crypto/src/uniffi_support.rs
+++ crates/bitwarden-crypto/src/uniffi_support.rs
@@ -1,45 +1,19 @@
 use std::{num::NonZeroU32, str::FromStr};
 
-use crate::{CryptoError, EncString, UniffiCustomTypeConverter, UnsignedSharedKey};
+use crate::{CryptoError, EncString, UnsignedSharedKey};
 
-uniffi::custom_type!(NonZeroU32, u32);
+uniffi::custom_type!(NonZeroU32, u32, {
+    remote,
+    lower: |obj| obj.get(),
+    try_lift: |val| Self::new(val).ok_or(CryptoError::ZeroNumber.into()),
+});
 
-impl UniffiCustomTypeConverter for NonZeroU32 {
-    type Builtin = u32;
+uniffi::custom_type!(EncString, String, {
+    lower: |obj| obj.to_string(),
+    try_lift: |val| val.parse().map_err(|e: CryptoError| e.into()),
+});
 
-    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
-        Self::new(val).ok_or(CryptoError::ZeroNumber.into())
-    }
-
-    fn from_custom(obj: Self) -> Self::Builtin {
-        obj.get()
-    }
-}
-
-uniffi::custom_type!(EncString, String);
-
-impl UniffiCustomTypeConverter for EncString {
-    type Builtin = String;
-
-    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
-        val.parse().map_err(|e: CryptoError| e.into())
-    }
-
-    fn from_custom(obj: Self) -> Self::Builtin {
-        obj.to_string()
-    }
-}
-
-uniffi::custom_type!(UnsignedSharedKey, String);
-
-impl UniffiCustomTypeConverter for UnsignedSharedKey {
-    type Builtin = String;
-
-    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
-        Self::from_str(&val).map_err(|e| e.into())
-    }
-
-    fn from_custom(obj: Self) -> Self::Builtin {
-        obj.to_string()
-    }
-}
+uniffi::custom_type!(UnsignedSharedKey, String, {
+    lower: |obj| obj.to_string(),
+    try_lift: |val| Self::from_str(&val).map_err(|e| e.into()),
+});
