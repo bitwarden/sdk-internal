d9c224bff969dabd9334451531e37a049b15d71f
Bernd Schoolmann <mail@quexten.com>
2025-05-29 10:20:59 +0200

    Remove allow

--- crates/bitwarden-crypto/src/store/context.rs
+++ crates/bitwarden-crypto/src/store/context.rs
@@ -346,7 +346,6 @@ impl<Ids: KeyIds> KeyStoreContext<'_, Ids> {
         .ok_or_else(|| crate::CryptoError::MissingKeyId(format!("{key_id:?}")))
     }
 
-    #[allow(unused)]
     fn get_signing_key(&self, key_id: Ids::Signing) -> Result<&SigningKey> {
         if key_id.is_local() {
             self.local_signing_keys.get(key_id)
