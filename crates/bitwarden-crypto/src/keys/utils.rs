f22531c3d5eecc4506b25b8149ba109734fd8d0b
Bernd Schoolmann <accounts@quexten.com>
2025-06-04 10:09:50 +0200

    Update crates/bitwarden-crypto/src/keys/utils.rs
    Co-authored-by: Matt Gibson <mgibson@bitwarden.com>

--- crates/bitwarden-crypto/src/keys/utils.rs
+++ crates/bitwarden-crypto/src/keys/utils.rs
@@ -27,7 +27,7 @@ pub(crate) fn pad_bytes(bytes: &mut Vec<u8>, min_length: usize) {
 }
 
 /// Unpads bytes that is padded using the PKCS7-like padding defined by [pad_bytes].
-/// The last N bytes of the padded bytes all have the value N.
+/// The last N bytes of the padded bytes all have the value N. Minimum of 1 padding byte.
 /// For example, padded to size 4, the value 0,0 becomes 0,0,2,2.
 pub(crate) fn unpad_bytes(padded_bytes: &[u8]) -> Result<&[u8], CryptoError> {
     let pad_len = *padded_bytes.last().ok_or(CryptoError::InvalidPadding)? as usize;
