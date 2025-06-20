e53023b2c9b27c1d15df9434e0369783ddd9a4e8
Daniel García <dani-garcia@users.noreply.github.com>
2025-06-20 15:02:02 +0200

    Wrap wasm-bindgen-cli

---
+++ crates/wasm-bindgen-cli-runner/wasm-bindgen-cli-runner.rs
@@ -0,0 +1,16 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<_> = std::env::args().skip(1).collect();
+
+    let [target, out_dir, input] = args
+        .try_into()
+        .expect("Usage: cargo run -p wasm-bindgen-cli-runner -- <TARGET> <OUT_DIR> <INPUT>");
+
+    let mut b = wasm_bindgen_cli_support::Bindgen::new();
+    match target.as_str() {
+        "bundler" => b.bundler(true)?,
+        "nodejs" => b.nodejs(true)?,
+        s => panic!("invalid target: `{s}`"),
+    };
+
+    b.input_path(input).generate(out_dir).map_err(Into::into)
+}
