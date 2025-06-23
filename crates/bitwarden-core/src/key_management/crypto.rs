469e2cbfde69278f23fac15f43ec57c0703e30e6
Bernd Schoolmann <mail@quexten.com>
2025-06-23 12:46:59 +0200

    Apply clippy fix

--- crates/bitwarden-core/src/key_management/crypto.rs
+++ crates/bitwarden-core/src/key_management/crypto.rs
@@ -579,7 +579,7 @@ pub(super) fn verify_asymmetric_keys(
     })
 }
 
-///
+/// Response for the `make_keys_for_user_crypto_v2`, containing a set of keys for a user
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 #[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
