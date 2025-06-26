81b5a15678da1953c86f9f2d1b8dc2c6c8dc240b
Bernd Schoolmann <accounts@quexten.com>
2025-06-26 17:14:46 +0200

    Update crates/bitwarden-core/src/client/encryption_settings.rs
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- crates/bitwarden-core/src/client/encryption_settings.rs
+++ crates/bitwarden-core/src/client/encryption_settings.rs
@@ -61,7 +61,7 @@ impl EncryptionSettings {
             let dec: Vec<u8> = private_key.decrypt_with_key(&user_key)?;
             // FIXME: [PM-11690] - Temporarily ignore invalid private keys until we have a recovery
             // process in place.
-            AsymmetricCryptoKey::from_der(&Pkcs8PrivateKeyBytes::from(dec))
+            AsymmetricCryptoKey::from_der(&dec.into())
                 .map_err(|_| {
                     warn!("Invalid private key");
                 })
