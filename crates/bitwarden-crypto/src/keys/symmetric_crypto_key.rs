6c8092ab336ac74ddd83804f048ce59e57888449
Bernd Schoolmann <accounts@quexten.com>
2025-06-26 18:41:27 +0200

    Update crates/bitwarden-crypto/src/keys/symmetric_crypto_key.rs
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- crates/bitwarden-crypto/src/keys/symmetric_crypto_key.rs
+++ crates/bitwarden-crypto/src/keys/symmetric_crypto_key.rs
@@ -155,7 +155,7 @@ impl SymmetricCryptoKey {
             EncodedSymmetricKey::CoseKey(_) => {
                 let mut encoded_key: Vec<u8> = encoded_key.into();
                 pad_key(&mut encoded_key, Self::AES256_CBC_HMAC_KEY_LEN + 1);
-                Bytes::from(encoded_key)
+                BitwardenLegacyKeyBytes::from(encoded_key)
             }
         }
     }
