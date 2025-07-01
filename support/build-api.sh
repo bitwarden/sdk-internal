3a285b9534995f4e23157e84aad7ad107074389f
Daniel García <dani-garcia@users.noreply.github.com>
2025-07-01 21:07:50 +0200

    Use request-trait template
    Also updated the request-trait templates to match the changes we'de done to reqwest:
    - Replace &str by uuids when appropiate
    - Removed lifetimes, which were unused
    
    Also updated the build script and added missing async-trait and mockall dependencies

--- support/build-api.sh
+++ support/build-api.sh
@@ -18,7 +18,7 @@ npx openapi-generator-cli generate \
     -o crates/bitwarden-api-api \
     --package-name bitwarden-api-api \
     -t ./support/openapi-template \
-    --additional-properties=packageVersion=$VERSION,packageDescription=\"Api bindings for the Bitwarden API.\"
+    --additional-properties=library=reqwest-trait,mockall,topLevelApiClient,packageVersion=$VERSION,packageDescription=\"Api bindings for the Bitwarden API.\"
 
 # Delete old directory to ensure all files are updated
 rm -rf crates/bitwarden-api-identity/src
@@ -30,7 +30,7 @@ npx openapi-generator-cli generate \
     -o crates/bitwarden-api-identity \
     --package-name bitwarden-api-identity \
     -t ./support/openapi-template \
-    --additional-properties=packageVersion=$VERSION,packageDescription=\"Api bindings for the Bitwarden Identity API.\"
+    --additional-properties=library=reqwest-trait,mockall,topLevelApiClient,packageVersion=$VERSION,packageDescription=\"Api bindings for the Bitwarden Identity API.\"
 
 rustup toolchain install nightly
 cargo +nightly fmt
