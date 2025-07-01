418e7714ea9ec15bf90ccf3dfdb1b60e9a93ef69
Daniel García <dani-garcia@users.noreply.github.com>
2025-07-01 21:07:50 +0200

    Use request-trait template

--- support/build-api.sh
+++ support/build-api.sh
@@ -18,7 +18,7 @@ npx openapi-generator-cli generate \
     -o crates/bitwarden-api-api \
     --package-name bitwarden-api-api \
     -t ./support/openapi-template \
-    --additional-properties=packageVersion=$VERSION,packageDescription=\"Api bindings for the Bitwarden API.\"
+    --additional-properties=library=reqwest-trait,mockall,topLevelApiClient,packageVersion=$VERSION,packageDescription=\"Api bindings for the Bitwarden API.\"
 
 # Delete old directory to ensure all files are updated
 rm -rf crates/bitwarden-api-identity/src
@@ -30,8 +30,23 @@ npx openapi-generator-cli generate \
     -o crates/bitwarden-api-identity \
     --package-name bitwarden-api-identity \
     -t ./support/openapi-template \
-    --additional-properties=packageVersion=$VERSION,packageDescription=\"Api bindings for the Bitwarden Identity API.\"
+    --additional-properties=library=reqwest-trait,mockall,topLevelApiClient,packageVersion=$VERSION,packageDescription=\"Api bindings for the Bitwarden Identity API.\"
 
 rustup toolchain install nightly
+
+# Rustfmt has what looks like a bug, where it requires multiple passes to format the code.
+# For example with code like this:
+# ```rust
+# /// Test Doc
+# ///
+# ///
+# fn test() {}
+# ```
+# The first pass will remove one of the empty lines but not the second one, so we need a
+# second pass to remove the second empty line. The swagger generated code adds three comment
+# lines, for path, description and notes and for us the last two are usually empty, which explains
+# the need for these two passes.
 cargo +nightly fmt
+cargo +nightly fmt
+
 npm run prettier
