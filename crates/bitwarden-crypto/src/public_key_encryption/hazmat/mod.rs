e8f222ac0c79d11ca312e8b354d95dce92712e1b
Bernd Schoolmann <mail@quexten.com>
2025-07-04 18:32:28 +0200

    Add comment

--- crates/bitwarden-crypto/src/public_key_encryption/hazmat/mod.rs
+++ crates/bitwarden-crypto/src/public_key_encryption/hazmat/mod.rs
@@ -1,2 +1,6 @@
+//! This module contains the low-level public-key encryption implementations.
+//! Any modifications to this module need to be most thoroughly reviewed.
+//!
+//! This module should only be referenced by the `public_key_encryption` module.
 mod rsa;
 pub(super) use rsa::encrypt_rsa2048_oaep_sha1;
