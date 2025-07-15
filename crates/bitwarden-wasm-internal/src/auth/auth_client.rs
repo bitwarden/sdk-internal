5cb35a9f096c21cac5bd36a0df124bc5b5749a1d
Jared Snider <jsnider@bitwarden.com>
2025-07-15 16:49:34 -0400

    PM-22661 - Add anonymous auth_client and documentation

---
+++ crates/bitwarden-wasm-internal/src/auth/auth_client.rs
@@ -0,0 +1,34 @@
+use crate::Client;
+
+/// Provides anonymous authentication / authorization methods.
+pub struct AuthClient {
+    /// The underlying client that this [`AuthClient`] uses.
+    /// pub(crate) dictates that the client is only accessible within the crate.
+    pub(crate) client: crate::Client,
+}
+
+impl Client {
+    /// Convenience method on [`Client`] to access the [`AuthClient`].
+    ///
+    /// # Example of usage
+    ///
+    /// ```rust
+    /// let client = Client::new(None);
+    /// let auth_client = client.auth();
+    /// ```
+    ///
+    /// # Example of usage without this method
+    /// ```rust
+    /// let auth_client = AuthClient {
+    ///     client: client.clone(),
+    /// };
+    /// ```
+    ///
+    /// # Returns
+    /// A new instance of [`AuthClient`].
+    pub fn auth(&self) -> AuthClient {
+        AuthClient {
+            client: self.clone(),
+        }
+    }
+}
