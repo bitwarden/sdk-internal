e20964643870a0291f31235e7bec1912470448ac
Jared Snider <jsnider@bitwarden.com>
2025-07-18 18:16:06 -0400

    PM-22661 - SendAccessTokenRequest - add comment

--- crates/bitwarden-wasm-internal/src/auth/send_access/requests/send_access_token_request.rs
+++ crates/bitwarden-wasm-internal/src/auth/send_access/requests/send_access_token_request.rs
@@ -1,7 +1,16 @@
 /// Go to parent and import the necessary modules.
 use super::super::enums::SendAccessCredentials;
 
+/// A request structure for requesting a send access token from the API.
+
+// TODO: figure out if I will need to use serde to expose this outside the crate
+// to the typescript layer.
+// Note: deny_unknown_fields instructs serde to error if any unknown fields are present in the JSON.
+// #[serde(rename_all = "camelCase", deny_unknown_fields)]
 pub struct SendAccessTokenRequest {
+    /// The id of the send for which the access token is requested.
     pub send_id: String,
+
+    /// The optional send access credentials.
     pub send_access_credentials: Option<SendAccessCredentials>,
 }
