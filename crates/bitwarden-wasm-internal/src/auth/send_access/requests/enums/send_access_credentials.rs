d91275a03385fdfc14fca7a1ef503f0c4446d7ed
Jared Snider <jsnider@bitwarden.com>
2025-07-21 16:44:31 -0400

    PM-22661 - Add clone to request and credentials for easier test usage

--- crates/bitwarden-wasm-internal/src/auth/send_access/requests/enums/send_access_credentials.rs
+++ crates/bitwarden-wasm-internal/src/auth/send_access/requests/enums/send_access_credentials.rs
@@ -1,24 +1,25 @@
 /// Credentials for sending password secured access requests.
-#[derive(serde::Serialize)]
+/// Clone auto implements the standard lib's Clone trait, allowing us to create copies of this struct.
+#[derive(serde::Serialize, Clone)]
 pub struct SendPasswordCredentials {
     pub password_hash: String,
 }
 
 /// Credentials for sending an OTP to the user's email address.
 /// This is used when the send requires email verification with an OTP.
-#[derive(serde::Serialize)]
+#[derive(serde::Serialize, Clone)]
 pub struct SendEmailCredentials {
     pub email: String,
 }
 
 /// Credentials for getting a send access token using an email and OTP.
-#[derive(serde::Serialize)]
+#[derive(serde::Serialize, Clone)]
 pub struct SendEmailOtpCredentials {
     pub email: String,
     pub otp: String,
 }
 
-#[derive(serde::Serialize)]
+#[derive(serde::Serialize, Clone)]
 // Use untagged so that each variant can be serialized without a type tag.
 // For example, this allows us to serialize the password credentials as just
 // {"password_hash": "value"} instead of {"type": "password", "password_hash": "value"}.
