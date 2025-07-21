1d7d6385f6c6134706c881fbe3435d33cdae89d2
Jared Snider <jsnider@bitwarden.com>
2025-07-21 11:45:22 -0400

    PM-22661 - Auth>SendAccess>Requests mod.rs - add enums module and make public.

--- crates/bitwarden-wasm-internal/src/auth/send_access/requests/mod.rs
+++ crates/bitwarden-wasm-internal/src/auth/send_access/requests/mod.rs
@@ -1,5 +1,7 @@
 mod send_access_token_payload;
 mod send_access_token_request;
 
+pub mod enums;
+
 pub use send_access_token_payload::SendAccessTokenPayload;
 pub use send_access_token_request::SendAccessTokenRequest;
