cc925bdf1d88ed5a70ddb3da899c77e4b629edcd
Jared Snider <jsnider@bitwarden.com>
2025-07-21 18:31:49 -0400

    PM-22661 - SendAccessTokenError - add tests

--- crates/bitwarden-wasm-internal/src/auth/send_access/responses/enums/send_access_token_error.rs
+++ crates/bitwarden-wasm-internal/src/auth/send_access/responses/enums/send_access_token_error.rs
@@ -7,3 +7,23 @@ pub enum SendAccessTokenError {
     #[serde(rename = "invalid_grant")]
     InvalidGrant,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deserialize_send_access_token_error_invalid_request() {
+        let error: String = "\"invalid_request\"".to_string();
+        let result: SendAccessTokenError = serde_json::from_str(&error).unwrap();
+
+        assert_eq!(result, SendAccessTokenError::InvalidRequest);
+    }
+
+    #[test]
+    fn test_deserialize_send_access_token_error_invalid_grant() {
+        let error: String = "\"invalid_grant\"".to_string();
+        let result: SendAccessTokenError = serde_json::from_str(&error).unwrap();
+        assert_eq!(result, SendAccessTokenError::InvalidGrant);
+    }
+}
