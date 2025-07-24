c620e4f84bd6482c626d1bb0634a0c6da4c45c01
Jared Snider <jsnider@bitwarden.com>
2025-07-24 18:35:00 -0400

    PM-22661 - Delete files that were moved to bw-auth crate

--- crates/bitwarden-wasm-internal/src/auth/send_access/requests/enums/mod.rs
+++
@@ -1,9 +0,0 @@
-mod send_access_client_type;
-mod send_access_credentials;
-mod send_access_token_payload_credentials;
-
-pub use send_access_client_type::SendAccessClientType;
-pub use send_access_credentials::{
-    SendAccessCredentials, SendEmailCredentials, SendEmailOtpCredentials, SendPasswordCredentials,
-};
-pub use send_access_token_payload_credentials::SendAccessTokenPayloadCredentials;
--- crates/bitwarden-wasm-internal/src/auth/send_access/requests/mod.rs
+++
@@ -1,7 +0,0 @@
-mod send_access_token_payload;
-mod send_access_token_request;
-
-pub mod enums;
-
-pub use send_access_token_payload::SendAccessTokenPayload;
-pub use send_access_token_request::SendAccessTokenRequest;
