fe238dec2c64870037c80f7d7dba08699a384fd9
Jared Snider <jsnider@bitwarden.com>
2025-07-24 17:24:58 -0400

    PM-22661 - SendAccess module - (1) declare internal module so send access can access its content but don't re-export (2) Add rest of public exports to token_request module.

--- crates/bitwarden-auth/src/send_access/mod.rs
+++ crates/bitwarden-auth/src/send_access/mod.rs
@@ -1,5 +1,9 @@
 mod client;
+mod internal; // don't make this public with a pub use to keep it internal
 mod token_request;
 
 pub use client::SendAccessClient;
-pub use token_request::{SendAccessCredentials, SendAccessTokenRequest};
+pub use token_request::{
+    SendAccessCredentials, SendAccessTokenRequest, SendEmailCredentials, SendEmailOtpCredentials,
+    SendPasswordCredentials,
+};
