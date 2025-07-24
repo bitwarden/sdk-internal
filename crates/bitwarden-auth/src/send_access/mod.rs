21842bf3f2ccb7c9e2fc686901c9ef7ce14799cc
Jared Snider <jsnider@bitwarden.com>
2025-07-24 18:39:50 -0400

    PM-22661 - token_request --> access_token_request

--- crates/bitwarden-auth/src/send_access/mod.rs
+++ crates/bitwarden-auth/src/send_access/mod.rs
@@ -1,9 +1,9 @@
+mod access_token_request;
 mod client;
 mod internal; // don't make this public with a pub use to keep it internal
-mod token_request;
 
-pub use client::SendAccessClient;
-pub use token_request::{
+pub use access_token_request::{
     SendAccessCredentials, SendAccessTokenRequest, SendEmailCredentials, SendEmailOtpCredentials,
     SendPasswordCredentials,
 };
+pub use client::SendAccessClient;
