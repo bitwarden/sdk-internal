c107751728d90577aa25d0fe5445313717059a00
Jared Snider <jsnider@bitwarden.com>
2025-07-24 19:25:46 -0400

    PM-22661 - SendAccessTokenError renamed to SendAccessTokenApiErrorResponse

--- crates/bitwarden-auth/src/send_access/internal/token_api_error_response.rs
+++ crates/bitwarden-auth/src/send_access/internal/token_api_error_response.rs
@@ -42,7 +42,7 @@ pub enum SendAccessTokenInvalidGrantError {
 #[serde(tag = "error", content = "error_description")]
 // ^ "error" becomes the variant discriminator which matches against the rename annotations; "error_description" is the payload for that variant which can be optional.
 /// Represents the possible errors that can occur when requesting a send access token.
-pub enum SendAccessTokenError {
+pub enum SendAccessTokenApiErrorResponse {
     #[serde(rename = "invalid_request")]
     /// Invalid request error, typically due to missing parameters for a specific
     /// credential flow. Ex. `send_id` is required.
@@ -142,25 +142,28 @@ mod tests {
         #[test]
         fn test_deserialize_send_access_token_error_invalid_request() {
             let obj = r#"{ "error": "invalid_request" }"#;
-            let result: SendAccessTokenError = serde_json::from_str(obj).unwrap();
-            assert_eq!(result, SendAccessTokenError::InvalidRequest(None));
+            let result: SendAccessTokenApiErrorResponse = serde_json::from_str(obj).unwrap();
+            assert_eq!(
+                result,
+                SendAccessTokenApiErrorResponse::InvalidRequest(None)
+            );
         }
 
         #[test]
         fn test_deserialize_send_access_token_error_invalid_grant() {
             let obj = r#"{ "error": "invalid_grant" }"#;
-            let result: SendAccessTokenError = serde_json::from_str(obj).unwrap();
-            assert_eq!(result, SendAccessTokenError::InvalidGrant(None));
+            let result: SendAccessTokenApiErrorResponse = serde_json::from_str(obj).unwrap();
+            assert_eq!(result, SendAccessTokenApiErrorResponse::InvalidGrant(None));
         }
 
         #[test]
         fn test_deserialize_send_access_token_error_invalid_request_with_details() {
             let obj =
                 r#"{ "error": "invalid_request", "error_description": "send_id is required." }"#;
-            let result: SendAccessTokenError = serde_json::from_str(obj).unwrap();
+            let result: SendAccessTokenApiErrorResponse = serde_json::from_str(obj).unwrap();
             assert_eq!(
                 result,
-                SendAccessTokenError::InvalidRequest(Some(
+                SendAccessTokenApiErrorResponse::InvalidRequest(Some(
                     SendAccessTokenInvalidRequestError::SendIdRequired
                 ))
             );
@@ -170,10 +173,10 @@ mod tests {
         fn test_deserialize_send_access_token_error_invalid_grant_with_details() {
             let obj =
                 r#"{ "error": "invalid_grant", "error_description": "Password_hash invalid." }"#;
-            let result: SendAccessTokenError = serde_json::from_str(obj).unwrap();
+            let result: SendAccessTokenApiErrorResponse = serde_json::from_str(obj).unwrap();
             assert_eq!(
                 result,
-                SendAccessTokenError::InvalidGrant(Some(
+                SendAccessTokenApiErrorResponse::InvalidGrant(Some(
                     SendAccessTokenInvalidGrantError::InvalidPasswordHash
                 ))
             );
