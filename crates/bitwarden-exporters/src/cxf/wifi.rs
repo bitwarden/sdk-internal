b1d13653173b449b43a04573d8cecdd825093985
Anders Åberg <anders@andersaberg.com>
2025-07-25 21:51:18 +0200

    Make it more idiomatic

--- crates/bitwarden-exporters/src/cxf/wifi.rs
+++ crates/bitwarden-exporters/src/cxf/wifi.rs
@@ -1,78 +1,63 @@
 use bitwarden_vault::FieldType;
-use credential_exchange_format::WifiCredential;
+use credential_exchange_format::{EditableFieldWifiNetworkSecurityType, WifiCredential};
 
 use crate::Field;
 
+/// Implement From for (name, value, field_type) tuples to make Field creation cleaner
+impl From<(&str, String, FieldType)> for Field {
+    fn from((name, value, field_type): (&str, String, FieldType)) -> Self {
+        Field {
+            name: Some(name.to_string()),
+            value: Some(value),
+            r#type: field_type as u8,
+            linked_id: None,
+        }
+    }
+}
+
 /// Convert WiFi credentials to custom fields following the CXF mapping convention
 pub fn wifi_to_fields(wifi: &WifiCredential) -> Vec<Field> {
-    let mut fields = Vec::new();
-
-    // SSID: Text field
-    if let Some(ssid) = &wifi.ssid {
-        fields.push(Field {
-            name: Some("SSID".to_string()),
-            value: Some(ssid.value.0.clone()),
-            r#type: FieldType::Text as u8,
-            linked_id: None,
-        });
-    }
-
-    // Passphrase: Hidden field (concealed-string)
-    if let Some(passphrase) = &wifi.passphrase {
-        fields.push(Field {
-            name: Some("Passphrase".to_string()),
-            value: Some(passphrase.value.0.clone()),
-            r#type: FieldType::Hidden as u8,
-            linked_id: None,
-        });
-    }
-
-    // Network Security Type: Text field
-    if let Some(security) = &wifi.network_security_type {
-        let security_str = match &security.value {
-            credential_exchange_format::EditableFieldWifiNetworkSecurityType::Unsecured => {
-                "Unsecured"
-            }
-            credential_exchange_format::EditableFieldWifiNetworkSecurityType::WpaPersonal => {
-                "WPA Personal"
-            }
-            credential_exchange_format::EditableFieldWifiNetworkSecurityType::Wpa2Personal => {
-                "WPA2 Personal"
-            }
-            credential_exchange_format::EditableFieldWifiNetworkSecurityType::Wpa3Personal => {
-                "WPA3 Personal"
-            }
-            credential_exchange_format::EditableFieldWifiNetworkSecurityType::Wep => "WEP",
-            credential_exchange_format::EditableFieldWifiNetworkSecurityType::Other(s) => s,
-        };
-        fields.push(Field {
-            name: Some("Network Security Type".to_string()),
-            value: Some(security_str.to_string()),
-            r#type: FieldType::Text as u8,
-            linked_id: None,
-        });
-    }
-
-    // Hidden: Boolean field
-    if let Some(hidden) = &wifi.hidden {
-        fields.push(Field {
-            name: Some("Hidden".to_string()),
-            value: Some(if hidden.value.0 { "true" } else { "false" }.to_string()),
-            r#type: FieldType::Boolean as u8,
-            linked_id: None,
-        });
-    }
-
-    fields
+    [
+        // SSID: Text field
+        wifi.ssid
+            .as_ref()
+            .map(|ssid| ("SSID", ssid.value.0.clone(), FieldType::Text).into()),
+        // Passphrase: Hidden field (concealed-string)
+        wifi.passphrase
+            .as_ref()
+            .map(|passphrase| ("Passphrase", passphrase.value.0.clone(), FieldType::Hidden).into()),
+        // Network Security Type: Text field
+        wifi.network_security_type.as_ref().map(|security| {
+            let security_str = match &security.value {
+                EditableFieldWifiNetworkSecurityType::Unsecured => "Unsecured",
+                EditableFieldWifiNetworkSecurityType::WpaPersonal => "WPA Personal",
+                EditableFieldWifiNetworkSecurityType::Wpa2Personal => "WPA2 Personal",
+                EditableFieldWifiNetworkSecurityType::Wpa3Personal => "WPA3 Personal",
+                EditableFieldWifiNetworkSecurityType::Wep => "WEP",
+                EditableFieldWifiNetworkSecurityType::Other(s) => s,
+            };
+            (
+                "Network Security Type",
+                security_str.to_string(),
+                FieldType::Text,
+            )
+                .into()
+        }),
+        // Hidden: Boolean field
+        wifi.hidden.as_ref().map(|hidden| {
+            let hidden_str = if hidden.value.0 { "true" } else { "false" };
+            ("Hidden", hidden_str.to_string(), FieldType::Boolean).into()
+        }),
+    ]
+    .into_iter()
+    .flatten()
+    .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use bitwarden_vault::FieldType;
-    use credential_exchange_format::{
-        EditableField, EditableFieldBoolean, EditableFieldConcealedString, EditableFieldString,
-        EditableFieldWifiNetworkSecurityType,
-    };
+    use credential_exchange_format::EditableFieldWifiNetworkSecurityType;
 
     use super::*;
 
@@ -92,31 +77,15 @@ mod tests {
         security_type: Option<EditableFieldWifiNetworkSecurityType>,
         hidden: Option<bool>,
     ) -> WifiCredential {
+        use credential_exchange_format::{
+            EditableFieldBoolean, EditableFieldConcealedString, EditableFieldString,
+        };
+
         WifiCredential {
-            ssid: ssid.map(|s| EditableField {
-                id: None,
-                value: EditableFieldString(s.to_string()),
-                label: None,
-                extensions: None,
-            }),
-            passphrase: passphrase.map(|p| EditableField {
-                id: None,
-                value: EditableFieldConcealedString(p.to_string()),
-                label: None,
-                extensions: None,
-            }),
-            network_security_type: security_type.map(|st| EditableField {
-                id: None,
-                value: st,
-                label: None,
-                extensions: None,
-            }),
-            hidden: hidden.map(|h| EditableField {
-                id: None,
-                value: EditableFieldBoolean(h),
-                label: None,
-                extensions: None,
-            }),
+            ssid: ssid.map(|s| EditableFieldString(s.to_string()).into()),
+            passphrase: passphrase.map(|p| EditableFieldConcealedString(p.to_string()).into()),
+            network_security_type: security_type.map(|st| st.into()),
+            hidden: hidden.map(|h| EditableFieldBoolean(h).into()),
         }
     }
 
