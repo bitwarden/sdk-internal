98f5d7b228a3bd0485814fe26a26baf8f0de172d
Jared Snider <jsnider@bitwarden.com>
2025-08-12 14:58:02 -0400

    PM-22661 - Try to appease the linter again with turning links into actual links.

--- crates/bitwarden-auth/src/send_access/api/token_api_success_response.rs
+++ crates/bitwarden-auth/src/send_access/api/token_api_success_response.rs
@@ -8,11 +8,11 @@ pub struct SendAccessTokenApiSuccessResponse {
     /// The duration in seconds until the token expires.
     pub expires_in: u64,
     /// The scope of the access token.
-    /// RFC: https://datatracker.ietf.org/doc/html/rfc6749#section-3.3
+    /// RFC: <https://datatracker.ietf.org/doc/html/rfc6749#section-3.3>
     pub scope: String,
     /// The type of the token.
     /// This will be "Bearer" for send access tokens.
     /// More information can be found in the OAuth 2.0 authZ framework RFC:
-    /// https://datatracker.ietf.org/doc/html/rfc6749#section-7.1
+    /// <https://datatracker.ietf.org/doc/html/rfc6749#section-7.1>
     pub token_type: String,
 }
