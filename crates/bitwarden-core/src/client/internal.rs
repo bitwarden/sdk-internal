1357d4beacf8806ef105afeb4cc96391385a94cb
Bernd Schoolmann <mail@quexten.com>
2025-08-14 14:25:47 +0200

    Cargo fmt

--- crates/bitwarden-core/src/client/internal.rs
+++ crates/bitwarden-core/src/client/internal.rs
@@ -326,7 +326,8 @@ impl InternalClient {
         key_state: UserKeyState,
     ) -> Result<(), EncryptionSettingsError> {
         let decrypted_user_key = {
-            // Note: This block ensures ctx is dropped. Otherwise it would cause a deadlock when initializing the user crypto
+            // Note: This block ensures ctx is dropped. Otherwise it would cause a deadlock when
+            // initializing the user crypto
             use crate::key_management::SymmetricKeyId;
             let ctx = &mut self.key_store.context_mut();
             let decrypted_user_key_id = pin_protected_user_key_envelope
