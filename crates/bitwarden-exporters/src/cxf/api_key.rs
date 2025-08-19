b8e494b74546b400dfa1fba4ddd1c6517922a2d6
Anders Åberg <anders@andersaberg.com>
2025-08-19 11:02:06 +0200

    PM-23639: Add CXF Import mappings (#361)
    Adds multiple CXF mappings

--- crates/bitwarden-exporters/src/cxf/api_key.rs
+++ crates/bitwarden-exporters/src/cxf/api_key.rs
@@ -3,7 +3,7 @@ use credential_exchange_format::ApiKeyCredential;
 use crate::{cxf::editable_field::create_field, Field};
 
 /// Convert API key credentials to custom fields
-pub fn api_key_to_fields(api_key: &ApiKeyCredential) -> Vec<Field> {
+pub(super) fn api_key_to_fields(api_key: &ApiKeyCredential) -> Vec<Field> {
     [
         api_key.key.as_ref().map(|key| create_field("API Key", key)),
         api_key
--- crates/bitwarden-exporters/src/lib.rs
+++ crates/bitwarden-exporters/src/lib.rs
@@ -280,7 +280,7 @@ pub enum SecureNoteType {
 }
 
 #[allow(missing_docs)]
-#[derive(Clone)]
+#[derive(Clone, Default, Debug, PartialEq)]
 pub struct Identity {
     pub title: Option<String>,
     pub first_name: Option<String>,
