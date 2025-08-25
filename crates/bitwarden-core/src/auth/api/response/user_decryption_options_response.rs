5e6f1dd04bb9d909c53cef0895e758172517ee74
Maciej Zieniuk <mzieniuk@bitwarden.com>
2025-08-25 18:20:05 +0100

    UserDecryptionOptions documentation

--- crates/bitwarden-core/src/auth/api/response/user_decryption_options_response.rs
+++ crates/bitwarden-core/src/auth/api/response/user_decryption_options_response.rs
@@ -1,8 +1,12 @@
 use bitwarden_api_api::models::MasterPasswordUnlockResponseModel;
 use serde::{Deserialize, Serialize};
 
+/// Provides user decryption options used to unlock user's vault.
+/// Currently, only master password unlock is supported.
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub(crate) struct UserDecryptionOptionsResponseModel {
+    /// Contains information needed to unlock user's vault with master password.
+    /// None when user have no master password.
     #[serde(
         rename = "masterPasswordUnlock",
         skip_serializing_if = "Option::is_none"
