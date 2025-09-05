4065a1fc47f57ac3034662937e3862d7dcd5d08b
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-09-05 16:09:57 +0200

    feat: rename to make it a drop-in replacement for the OSS version

--- bitwarden_license/bit-bitwarden-wasm-internal/build.sh
+++ bitwarden_license/bit-bitwarden-wasm-internal/build.sh
@@ -26,20 +26,20 @@ fi
 # this normally requires a nightly build, but we can also use the
 # RUSTC_BOOTSTRAP hack to use the same stable version as the normal build
 RUSTFLAGS=-Ctarget-cpu=mvp RUSTC_BOOTSTRAP=1 cargo build -p bit-bitwarden-wasm-internal -Zbuild-std=panic_abort,std --target wasm32-unknown-unknown ${RELEASE_FLAG}
-wasm-bindgen --target bundler --out-dir bitwarden_license/bit-bitwarden-wasm-internal/npm ./target/wasm32-unknown-unknown/${BUILD_FOLDER}/bit_bitwarden_wasm_internal.wasm
-wasm-bindgen --target nodejs --out-dir bitwarden_license/bit-bitwarden-wasm-internal/npm/node ./target/wasm32-unknown-unknown/${BUILD_FOLDER}/bit_bitwarden_wasm_internal.wasm
+wasm-bindgen --target bundler --out-name bitwarden_wasm_internal --out-dir bitwarden_license/bit-bitwarden-wasm-internal/npm ./target/wasm32-unknown-unknown/${BUILD_FOLDER}/bitwarden_wasm_internal.wasm
+wasm-bindgen --target nodejs --out-name bitwarden_wasm_internal --out-dir bitwarden_license/bit-bitwarden-wasm-internal/npm/node ./target/wasm32-unknown-unknown/${BUILD_FOLDER}/bitwarden_wasm_internal.wasm
 
 # Format
 npx prettier --write ./bitwarden_license/bit-bitwarden-wasm-internal/npm
 
 # Optimize size
-wasm-opt -Os ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bit_bitwarden_wasm_internal_bg.wasm -o ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bit_bitwarden_wasm_internal_bg.wasm
-wasm-opt -Os ./bitwarden_license/bit-bitwarden-wasm-internal/npm/node/bit_bitwarden_wasm_internal_bg.wasm -o ./bitwarden_license/bit-bitwarden-wasm-internal/npm/node/bit_bitwarden_wasm_internal_bg.wasm
+wasm-opt -Os ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm -o ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm
+wasm-opt -Os ./bitwarden_license/bit-bitwarden-wasm-internal/npm/node/bitwarden_wasm_internal_bg.wasm -o ./bitwarden_license/bit-bitwarden-wasm-internal/npm/node/bitwarden_wasm_internal_bg.wasm
 
 # Transpile to JS
-wasm2js -Os ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bit_bitwarden_wasm_internal_bg.wasm -o ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bit_bitwarden_wasm_internal_bg.wasm.js
-npx terser ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bit_bitwarden_wasm_internal_bg.wasm.js -o ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bit_bitwarden_wasm_internal_bg.wasm.js
+wasm2js -Os ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm -o ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm.js
+npx terser ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm.js -o ./bitwarden_license/bit-bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm.js
 
 # Typecheck the generated TypeScript definitions
 cd bitwarden_license/bit-bitwarden-wasm-internal/npm
-npx tsc --noEmit --lib es2020,dom bit_bitwarden_wasm_internal.d.ts
+npx tsc --noEmit --lib es2020,dom bitwarden_wasm_internal.d.ts
