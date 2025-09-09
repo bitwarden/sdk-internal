a636111d422de45c3c8bcc90c7095bddd15091d1
Jared Snider <jsnider@bitwarden.com>
2025-09-09 09:18:40 -0400

    PM-22661 - Per PR feedback, move module docs into actual mod.rs file and use inner doc comments

--- crates/bitwarden-auth/src/lib.rs
+++ crates/bitwarden-auth/src/lib.rs
@@ -3,7 +3,6 @@
 mod auth_client;
 mod common;
 
-/// Module for handling Send Access token requests and responses.
 pub mod send_access;
 
 pub use auth_client::{AuthClient, AuthClientExt};
--- crates/bitwarden-auth/src/send_access/mod.rs
+++ crates/bitwarden-auth/src/send_access/mod.rs
@@ -1,3 +1,13 @@
+//! The SendAccess module handles send access token requests and responses.
+//! We use a custom extension OAuth2 grant type to request send access tokens
+//! outside the context of a Bitwarden user. This will be used by the send portion of the
+//! Bitwarden web app to allow users to access send access functionality without
+//! needing to log in to a Bitwarden account.
+//! Sends can be anonymous, password protected, or email protected.
+//! If you request an access token for an anonymous send by id, no credentials are required.
+//! If you request an access token for a password protected send, you must provide a correct
+//! password hash. If you request an access token for an email protected send, you must provide the
+//! email address and a one-time passcode (OTP) sent to that email address.
 mod access_token_request;
 mod access_token_response;
 mod client;
