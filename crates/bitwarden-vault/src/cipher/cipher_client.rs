1ff9467de1d77df8028fb19079df5b7cd1d62a63
Nik Gilmore <ngilmore@bitwarden.com>
2025-09-10 16:57:30 -0700

    Move extract_cipher_types to be a function on Cipher

--- crates/bitwarden-vault/src/cipher/cipher_client.rs
+++ crates/bitwarden-vault/src/cipher/cipher_client.rs
@@ -350,6 +350,7 @@ mod tests {
                 deleted_date: None,
                 revision_date: "2024-05-31T09:35:55.12Z".parse().unwrap(),
                 archived_date: None,
+                data: None,
             }])
 
             .unwrap();
