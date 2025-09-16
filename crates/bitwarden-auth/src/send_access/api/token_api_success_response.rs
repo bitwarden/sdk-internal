68aa1aaec0897b9de1d2c2c1b39d47692367a152
Jared Snider <116684653+JaredSnider-Bitwarden@users.noreply.github.com>
2025-09-16 08:31:05 -0400

    PM-22661 - Update crates/bitwarden-auth/src/send_access/api/token_api_success_response.rs
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- crates/bitwarden-auth/src/send_access/api/token_api_success_response.rs
+++ crates/bitwarden-auth/src/send_access/api/token_api_success_response.rs
@@ -2,17 +2,17 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 /// The server response for successful send access token request.
-pub struct SendAccessTokenApiSuccessResponse {
+pub(crate) struct SendAccessTokenApiSuccessResponse {
     /// The access token string.
-    pub access_token: String,
+    pub(crate) access_token: String,
     /// The duration in seconds until the token expires.
-    pub expires_in: u64,
+    pub(crate) expires_in: u64,
     /// The scope of the access token.
     /// RFC: <https://datatracker.ietf.org/doc/html/rfc6749#section-3.3>
-    pub scope: String,
+    pub(crate) scope: String,
     /// The type of the token.
     /// This will be "Bearer" for send access tokens.
     /// More information can be found in the OAuth 2.0 authZ framework RFC:
     /// <https://datatracker.ietf.org/doc/html/rfc6749#section-7.1>
-    pub token_type: String,
+    pub(crate) token_type: String,
 }
