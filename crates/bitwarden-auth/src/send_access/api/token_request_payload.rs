4814b16b115f0a5a2c04d03e2cc828b3283d1e00
Jared Snider <116684653+JaredSnider-Bitwarden@users.noreply.github.com>
2025-09-16 08:31:24 -0400

    PM-22661 - Update crates/bitwarden-auth/src/send_access/api/token_request_payload.rs
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- crates/bitwarden-auth/src/send_access/api/token_request_payload.rs
+++ crates/bitwarden-auth/src/send_access/api/token_request_payload.rs
@@ -51,7 +51,7 @@ impl From<Option<SendAccessCredentials>> for SendAccessTokenPayloadCredentials {
 /// Enum representing the type of client requesting a send access token.
 /// Eventually, this could / should be merged with the existing `ClientType` enum
 #[derive(Serialize, Deserialize, Debug)]
-pub enum SendAccessClientType {
+pub(crate) enum SendAccessClientType {
     /// Represents a Send client.
     /// This is a standalone client that lives within the BW web app, but has no context of a BW
     /// user.
