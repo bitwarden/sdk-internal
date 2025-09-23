c5ae947f5713f5a40679d6b901767aac8fe2dbb0
Hinton <hinton@users.noreply.github.com>
2025-09-23 19:48:07 +0200

    Expose ClientManagedTokens to mobile

--- crates/bitwarden-uniffi/src/lib.rs
+++ crates/bitwarden-uniffi/src/lib.rs
@@ -2,8 +2,10 @@
 
 uniffi::setup_scaffolding!();
 
+use std::sync::Arc;
+
 use auth::AuthClient;
-use bitwarden_core::ClientSettings;
+use bitwarden_core::{client::internal::ClientManagedTokens, ClientSettings};
 
 #[allow(missing_docs)]
 pub mod auth;
@@ -35,14 +37,20 @@ pub struct Client(pub(crate) bitwarden_pm::PasswordManagerClient);
 impl Client {
     /// Initialize a new instance of the SDK client
     #[uniffi::constructor]
-    pub fn new(settings: Option<ClientSettings>) -> Self {
+    pub fn new(
+        token_provider: Arc<dyn ClientManagedTokens>,
+        settings: Option<ClientSettings>,
+    ) -> Self {
         init_logger();
         setup_error_converter();
 
         #[cfg(target_os = "android")]
         android_support::init();
 
-        Self(bitwarden_pm::PasswordManagerClient::new(settings))
+        Self(bitwarden_pm::PasswordManagerClient::new_with_client_tokens(
+            settings,
+            token_provider,
+        ))
     }
 
     /// Crypto operations
