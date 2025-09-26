532a6ba0c6a62edbb8d1880c9b72baa1cdb17a57
Nik Gilmore <nik@nikgilmore.com>
2025-09-26 15:27:17 -0700

    Remove outdated comment from cipher_client/create.rs
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- crates/bitwarden-vault/src/cipher/cipher_client/create.rs
+++ crates/bitwarden-vault/src/cipher/cipher_client/create.rs
@@ -46,8 +46,6 @@ pub enum CreateCipherError {
 #[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
 #[cfg_attr(feature = "wasm", derive(Tsify), tsify(into_wasm_abi, from_wasm_abi))]
 pub struct CipherCreateRequest {
-    /// The ID of the user that is encrypting the cipher - this should always match the user
-    /// calling the API.
     pub organization_id: Option<OrganizationId>,
     pub folder_id: Option<FolderId>,
     pub name: String,
