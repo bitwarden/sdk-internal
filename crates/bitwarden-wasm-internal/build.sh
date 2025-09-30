046d7259b0326e04d6ca5c572047a85c8aed1596
Andreas Coroiu <andreas.coroiu@gmail.com>
2025-09-30 14:58:34 +0200

    feat: build commercial npm package

--- crates/bitwarden-wasm-internal/build.sh
+++ crates/bitwarden-wasm-internal/build.sh
@@ -12,6 +12,7 @@ git rev-parse HEAD > ./crates/bitwarden-wasm-internal/npm/VERSION
 
 # Parse flags
 ENABLE_LICENSE_FEATURE=""
+NPM_FOLDER="npm"
 RELEASE_FLAG=""
 BUILD_FOLDER="debug"
 
@@ -19,6 +20,7 @@ while [[ $# -gt 0 ]]; do
   case "$1" in
     -b)
       ENABLE_LICENSE_FEATURE="--features bitwarden-license"
+      NPM_FOLDER="bitwarden_license/npm"
       ;;
     -r)
       RELEASE_FLAG="--release"
@@ -45,20 +47,20 @@ fi
 # this normally requires a nightly build, but we can also use the
 # RUSTC_BOOTSTRAP hack to use the same stable version as the normal build
 RUSTFLAGS=-Ctarget-cpu=mvp RUSTC_BOOTSTRAP=1 cargo build -p bitwarden-wasm-internal -Zbuild-std=panic_abort,std --target wasm32-unknown-unknown ${RELEASE_FLAG} ${ENABLE_LICENSE_FEATURE}
-wasm-bindgen --target bundler --out-dir crates/bitwarden-wasm-internal/npm ./target/wasm32-unknown-unknown/${BUILD_FOLDER}/bitwarden_wasm_internal.wasm
-wasm-bindgen --target nodejs --out-dir crates/bitwarden-wasm-internal/npm/node ./target/wasm32-unknown-unknown/${BUILD_FOLDER}/bitwarden_wasm_internal.wasm
+wasm-bindgen --target bundler --out-dir crates/bitwarden-wasm-internal/${NPM_FOLDER} ./target/wasm32-unknown-unknown/${BUILD_FOLDER}/bitwarden_wasm_internal.wasm
+wasm-bindgen --target nodejs --out-dir crates/bitwarden-wasm-internal/${NPM_FOLDER}/node ./target/wasm32-unknown-unknown/${BUILD_FOLDER}/bitwarden_wasm_internal.wasm
 
 # Format
-npx prettier --write ./crates/bitwarden-wasm-internal/npm
+npx prettier --write ./crates/bitwarden-wasm-internal/${NPM_FOLDER}
 
 # Optimize size
-wasm-opt -Os ./crates/bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm -o ./crates/bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm
-wasm-opt -Os ./crates/bitwarden-wasm-internal/npm/node/bitwarden_wasm_internal_bg.wasm -o ./crates/bitwarden-wasm-internal/npm/node/bitwarden_wasm_internal_bg.wasm
+wasm-opt -Os ./crates/bitwarden-wasm-internal/${NPM_FOLDER}/bitwarden_wasm_internal_bg.wasm -o ./crates/bitwarden-wasm-internal/${NPM_FOLDER}/bitwarden_wasm_internal_bg.wasm
+wasm-opt -Os ./crates/bitwarden-wasm-internal/${NPM_FOLDER}/node/bitwarden_wasm_internal_bg.wasm -o ./crates/bitwarden-wasm-internal/${NPM_FOLDER}/node/bitwarden_wasm_internal_bg.wasm
 
 # Transpile to JS
-wasm2js -Os ./crates/bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm -o ./crates/bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm.js
-npx terser ./crates/bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm.js -o ./crates/bitwarden-wasm-internal/npm/bitwarden_wasm_internal_bg.wasm.js
+wasm2js -Os ./crates/bitwarden-wasm-internal/${NPM_FOLDER}/bitwarden_wasm_internal_bg.wasm -o ./crates/bitwarden-wasm-internal/${NPM_FOLDER}/bitwarden_wasm_internal_bg.wasm.js
+npx terser ./crates/bitwarden-wasm-internal/${NPM_FOLDER}/bitwarden_wasm_internal_bg.wasm.js -o ./crates/bitwarden-wasm-internal/${NPM_FOLDER}/bitwarden_wasm_internal_bg.wasm.js
 
 # Typecheck the generated TypeScript definitions
-cd crates/bitwarden-wasm-internal/npm
+cd crates/bitwarden-wasm-internal/${NPM_FOLDER}
 npx tsc --noEmit --lib es2020,dom bitwarden_wasm_internal.d.ts
