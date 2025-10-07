eabb6cfc1fa4fd92da4c268481295cd49e024807
Isaiah Inuwa <iinuwa@bitwarden.com>
2025-10-07 11:20:42 -0500

    Clarify implicit PRF truncation

--- crates/bitwarden-crypto/src/keys/prf.rs
+++ crates/bitwarden-crypto/src/keys/prf.rs
@@ -1,11 +1,14 @@
 use crate::{CryptoError, SymmetricCryptoKey, utils::stretch_key};
 
-/// Takes the output of a PRF and derives a symmetric key
+/// Takes the output of a PRF and derives a symmetric key.
+///
+/// The PRF output must be at least 32 bytes long.
 pub fn derive_symmetric_key_from_prf(prf: &[u8]) -> Result<SymmetricCryptoKey, CryptoError> {
     let (secret, _) = prf
         .split_at_checked(32)
         .ok_or_else(|| CryptoError::InvalidKeyLen)?;
     let secret: [u8; 32] = secret.try_into().unwrap();
+    // Don't allow uninitialized PRFs
     if secret.iter().all(|b| *b == b'\0') {
         return Err(CryptoError::ZeroNumber);
     }
@@ -17,28 +20,39 @@ pub fn derive_symmetric_key_from_prf(prf: &[u8]) -> Result<SymmetricCryptoKey, C
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_prf_succeeds() {
-        let prf = [
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-            24, 25, 26, 27, 28, 29, 30, 31,
-        ];
-        derive_symmetric_key_from_prf(&prf).unwrap();
+        let prf = pseudorandom_bytes(32);
+        let key = derive_symmetric_key_from_prf(&prf).unwrap();
+        assert!(matches!(key, SymmetricCryptoKey::Aes256CbcHmacKey(_)));
     }
 
     #[test]
     fn test_zero_key_fails() {
-        let prf = [
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0,
-        ];
+        let prf: Vec<u8> = (0..32).map(|_| 0).collect();
         let err = derive_symmetric_key_from_prf(&prf).unwrap_err();
         assert!(matches!(err, CryptoError::ZeroNumber));
     }
+
     #[test]
     fn test_short_prf_fails() {
-        let prf = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let prf = pseudorandom_bytes(9);
         let err = derive_symmetric_key_from_prf(&prf).unwrap_err();
         assert!(matches!(err, CryptoError::InvalidKeyLen));
+    }
+
+    #[test]
+    fn test_long_prf_truncated_to_proper_length() {
+        let long_prf = pseudorandom_bytes(33);
+        let prf = pseudorandom_bytes(32);
+        let key1 = derive_symmetric_key_from_prf(&long_prf).unwrap();
+        let key2 = derive_symmetric_key_from_prf(&prf).unwrap();
+        assert_eq!(key1, key2);
+    }
+
+    /// This returns the same bytes deterministically for a given length.
+    fn pseudorandom_bytes(len: usize) -> Vec<u8> {
+        (0..len).map(|x| (x % 255) as u8).collect()
     }
 }
