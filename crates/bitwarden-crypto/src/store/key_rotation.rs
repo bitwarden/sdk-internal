748f6d64f705be7bf87752b45b6bb51feb2f0fd2
Isaiah Inuwa <iinuwa@bitwarden.com>
2025-10-07 12:56:22 -0500

    Use upstream/downstream key terminology for rotateable key set

--- crates/bitwarden-crypto/src/store/key_rotation.rs
+++ crates/bitwarden-crypto/src/store/key_rotation.rs
@@ -48,16 +48,16 @@ pub fn dangerous_get_v2_rotated_account_keys<Ids: KeyIds>(
     })
 }
 
-/// A set of keys where a given `EncryptionKey` is protected by an encrypted public/private
-/// key-pair. The `EncryptionKey` is used to encrypt/decrypt data, while the public/private key-pair
-/// is used to rotate the `EncryptionKey`.
+/// A set of keys where a given `DownstreamKey` is protected by an encrypted public/private
+/// key-pair. The `DownstreamKey` is used to encrypt/decrypt data, while the public/private key-pair
+/// is used to rotate the `DownstreamKey`.
 ///
-/// The `PrivateKey` is protected by an `ExternalKey`, such as a `DeviceKey`, or `PrfKey`,
-/// and the `PublicKey` is protected by the `EncryptionKey`. This setup allows:
+/// The `PrivateKey` is protected by an `UpstreamKey`, such as a `DeviceKey`, or `PrfKey`,
+/// and the `PublicKey` is protected by the `DownstreamKey`. This setup allows:
 ///
-///   - Access to `EncryptionKey` by knowing the `ExternalKey`
-///   - Rotation to a `NewEncryptionKey` by knowing the current `EncryptionKey`, without needing access to
-///     the `ExternalKey`
+///   - Access to `DownstreamKey` by knowing the `UpstreamKey`
+///   - Rotation to a `NewDownstreamKey` by knowing the current `DownstreamKey`, without needing access to
+///     the `UpstreamKey`
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 #[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
@@ -67,21 +67,21 @@ pub fn dangerous_get_v2_rotated_account_keys<Ids: KeyIds>(
     tsify(into_wasm_abi, from_wasm_abi)
 )]
 pub struct RotateableKeySet {
-    /// `EncryptionKey` protected by encapsulation key
-    encapsulated_encryption_key: UnsignedSharedKey,
-    /// Encapsulation key protected by `EncryptionKey`
+    /// `DownstreamKey` protected by encapsulation key
+    encapsulated_downstream_key: UnsignedSharedKey,
+    /// Encapsulation key protected by `DownstreamKey`
     encrypted_encapsulation_key: EncString,
-    /// Decapsulation key protected by `ExternalKey`
-    wrapped_decapsulation_key: EncString,
+    /// Decapsulation key protected by `UpstreamKey`
+    encrypted_decapsulation_key: EncString,
 }
 
 impl RotateableKeySet {
-    /// Create a set of keys to allow access to the user key via the provided
-    /// symmetric wrapping key while allowing the user key to be rotated.
+    /// Create a set of keys to allow access to the downstream key via the provided
+    /// upstream key while allowing the downstream key to be rotated.
     pub fn new<Ids: KeyIds>(
         ctx: &KeyStoreContext<Ids>,
-        wrapping_key: &SymmetricCryptoKey,
-        key_to_wrap: Ids::Symmetric,
+        upstream_key: &SymmetricCryptoKey,
+        downstream_key_id: Ids::Symmetric,
     ) -> Result<Self, CryptoError> {
         let key_pair = AsymmetricCryptoKey::make(crate::PublicKeyEncryptionAlgorithm::RsaOaepSha1);
 
@@ -89,30 +89,28 @@ impl RotateableKeySet {
         // rather than the key store context because we don't want the keys to
         // wind up being stored in the borrowed context.
         #[allow(deprecated)]
-        let key_to_wrap_instance = ctx.dangerous_get_symmetric_key(key_to_wrap)?;
-        // encapsulate encryption key
-        let encapsulated_encryption_key = UnsignedSharedKey::encapsulate_key_unsigned(
-            key_to_wrap_instance,
-            &key_pair.to_public_key(),
-        )?;
+        let downstream_key = ctx.dangerous_get_symmetric_key(downstream_key_id)?;
+        // encapsulate downstream key
+        let encapsulated_downstream_key =
+            UnsignedSharedKey::encapsulate_key_unsigned(downstream_key, &key_pair.to_public_key())?;
 
-        // wrap decapsulation key
-        let wrapped_decapsulation_key = key_pair.to_der()?.encrypt_with_key(wrapping_key)?;
+        // wrap decapsulation key with upstream key
+        let encrypted_decapsulation_key = key_pair.to_der()?.encrypt_with_key(upstream_key)?;
 
-        // wrap encapsulation key with encryption key
+        // wrap encapsulation key with downstream key
         // Note: Usually, a public key is - by definition - public, so this should not be necessary.
         // The specific use-case for this function is to enable rotateable key sets, where
         // the "public key" is not public, with the intent of preventing the server from being able
-        // to overwrite the user key unlocked by the rotateable keyset.
+        // to overwrite the downstream key unlocked by the rotateable keyset.
         let encrypted_encapsulation_key = key_pair
             .to_public_key()
             .to_der()?
-            .encrypt_with_key(&key_to_wrap_instance)?;
+            .encrypt_with_key(&downstream_key)?;
 
         Ok(RotateableKeySet {
-            encapsulated_encryption_key,
+            encapsulated_downstream_key,
             encrypted_encapsulation_key,
-            wrapped_decapsulation_key,
+            encrypted_decapsulation_key,
         })
     }
 
@@ -122,19 +120,19 @@ impl RotateableKeySet {
     fn unlock<Ids: KeyIds>(
         &self,
         ctx: &mut KeyStoreContext<Ids>,
-        unwrapping_key: &SymmetricCryptoKey,
-        key_to_unwrap: Ids::Symmetric,
+        upstream_key: &SymmetricCryptoKey,
+        downstream_key_id: Ids::Symmetric,
     ) -> Result<(), CryptoError> {
         let priv_key_bytes: Vec<u8> = self
-            .wrapped_decapsulation_key
-            .decrypt_with_key(unwrapping_key)?;
+            .encrypted_decapsulation_key
+            .decrypt_with_key(upstream_key)?;
         let decapsulation_key =
             AsymmetricCryptoKey::from_der(&Pkcs8PrivateKeyBytes::from(priv_key_bytes))?;
-        let encryption_key = self
-            .encapsulated_encryption_key
+        let downstream_key = self
+            .encapsulated_downstream_key
             .decapsulate_key_unsigned(&decapsulation_key)?;
         #[allow(deprecated)]
-        ctx.set_symmetric_key(key_to_unwrap, encryption_key)?;
+        ctx.set_symmetric_key(downstream_key_id, downstream_key)?;
         Ok(())
     }
 }
@@ -143,26 +141,26 @@ impl RotateableKeySet {
 fn rotate_key_set<Ids: KeyIds>(
     ctx: &KeyStoreContext<Ids>,
     key_set: RotateableKeySet,
-    old_encryption_key_id: Ids::Symmetric,
-    new_encryption_key_id: Ids::Symmetric,
+    old_downstream_key_id: Ids::Symmetric,
+    new_downstream_key_id: Ids::Symmetric,
 ) -> Result<RotateableKeySet, CryptoError> {
     let pub_key_bytes = ctx.decrypt_data_with_symmetric_key(
-        old_encryption_key_id,
+        old_downstream_key_id,
         &key_set.encrypted_encapsulation_key,
     )?;
     let pub_key = SpkiPublicKeyBytes::from(pub_key_bytes);
     let encapsulation_key = AsymmetricPublicCryptoKey::from_der(&pub_key)?;
     // TODO: There is no method to store only the public key in the store, so we
-    // have pull out the encryption key to encapsulate it manually.
+    // have pull out the downstream key to encapsulate it manually.
     #[allow(deprecated)]
-    let new_encryption_key = ctx.dangerous_get_symmetric_key(new_encryption_key_id)?;
+    let new_downstream_key = ctx.dangerous_get_symmetric_key(new_downstream_key_id)?;
     let new_encapsulated_key =
-        UnsignedSharedKey::encapsulate_key_unsigned(new_encryption_key, &encapsulation_key)?;
-    let new_encrypted_encapsulation_key = pub_key.encrypt_with_key(new_encryption_key)?;
+        UnsignedSharedKey::encapsulate_key_unsigned(new_downstream_key, &encapsulation_key)?;
+    let new_encrypted_encapsulation_key = pub_key.encrypt_with_key(new_downstream_key)?;
     Ok(RotateableKeySet {
-        encapsulated_encryption_key: new_encapsulated_key,
+        encapsulated_downstream_key: new_encapsulated_key,
         encrypted_encapsulation_key: new_encrypted_encapsulation_key,
-        wrapped_decapsulation_key: key_set.wrapped_decapsulation_key,
+        encrypted_decapsulation_key: key_set.encrypted_decapsulation_key,
     })
 }
 
@@ -262,75 +260,75 @@ mod tests {
     #[test]
     fn test_rotateable_key_set_can_unlock() {
         // generate initial keys
-        let external_key = SymmetricCryptoKey::make_aes256_cbc_hmac_key();
+        let upstream_key = SymmetricCryptoKey::make_aes256_cbc_hmac_key();
         // set up store
         let store: KeyStore<TestIds> = KeyStore::default();
         let mut ctx = store.context_mut();
-        let original_encryption_key_id = TestSymmKey::A(0);
-        ctx.generate_symmetric_key(original_encryption_key_id)
+        let original_downstream_key_id = TestSymmKey::A(0);
+        ctx.generate_symmetric_key(original_downstream_key_id)
             .unwrap();
 
         // create key set
         let key_set =
-            RotateableKeySet::new(&ctx, &external_key, original_encryption_key_id).unwrap();
+            RotateableKeySet::new(&ctx, &upstream_key, original_downstream_key_id).unwrap();
 
         // unlock key set
-        let unwrapped_encryption_key_id = TestSymmKey::A(1);
+        let unwrapped_downstream_key_id = TestSymmKey::A(1);
         key_set
-            .unlock(&mut ctx, &external_key, unwrapped_encryption_key_id)
+            .unlock(&mut ctx, &upstream_key, unwrapped_downstream_key_id)
             .unwrap();
 
         #[allow(deprecated)]
-        let original_key = ctx
-            .dangerous_get_symmetric_key(original_encryption_key_id)
+        let original_downstream_key = ctx
+            .dangerous_get_symmetric_key(original_downstream_key_id)
             .unwrap();
         #[allow(deprecated)]
-        let unwrapped_key = ctx
-            .dangerous_get_symmetric_key(unwrapped_encryption_key_id)
+        let unwrapped_downstream_key = ctx
+            .dangerous_get_symmetric_key(unwrapped_downstream_key_id)
             .unwrap();
-        assert_eq!(original_key, unwrapped_key);
+        assert_eq!(original_downstream_key, unwrapped_downstream_key);
     }
 
     #[test]
     fn test_rotateable_key_set_rotation() {
         // generate initial keys
-        let external_key = SymmetricCryptoKey::make_aes256_cbc_hmac_key();
+        let upstream_key = SymmetricCryptoKey::make_aes256_cbc_hmac_key();
         // set up store
         let store: KeyStore<TestIds> = KeyStore::default();
         let mut ctx = store.context_mut();
-        let original_encryption_key_id = TestSymmKey::A(1);
-        ctx.generate_symmetric_key(original_encryption_key_id)
+        let original_downstream_key_id = TestSymmKey::A(1);
+        ctx.generate_symmetric_key(original_downstream_key_id)
             .unwrap();
 
         // create key set
         let key_set =
-            RotateableKeySet::new(&ctx, &external_key, original_encryption_key_id).unwrap();
+            RotateableKeySet::new(&ctx, &upstream_key, original_downstream_key_id).unwrap();
 
         // rotate
-        let new_encryption_key_id = TestSymmKey::A(2_1);
-        ctx.generate_symmetric_key(new_encryption_key_id).unwrap();
+        let new_downstream_key_id = TestSymmKey::A(2_1);
+        ctx.generate_symmetric_key(new_downstream_key_id).unwrap();
         let new_key_set = rotate_key_set(
             &ctx,
             key_set,
-            original_encryption_key_id,
-            new_encryption_key_id,
+            original_downstream_key_id,
+            new_downstream_key_id,
         )
         .unwrap();
 
         // After rotation, the new key set should be unlocked by the same
-        // external key and return the new encryption key.
-        let unwrapped_encryption_key_id = TestSymmKey::A(2_2);
+        // upstream key and return the new downstream key.
+        let unwrapped_downstream_key_id = TestSymmKey::A(2_2);
         new_key_set
-            .unlock(&mut ctx, &external_key, unwrapped_encryption_key_id)
+            .unlock(&mut ctx, &upstream_key, unwrapped_downstream_key_id)
             .unwrap();
         #[allow(deprecated)]
-        let new_encryption_key = ctx
-            .dangerous_get_symmetric_key(new_encryption_key_id)
+        let new_downstream_key = ctx
+            .dangerous_get_symmetric_key(new_downstream_key_id)
             .unwrap();
         #[allow(deprecated)]
-        let unwrapped_encryption_key = ctx
-            .dangerous_get_symmetric_key(unwrapped_encryption_key_id)
+        let unwrapped_downstream_key = ctx
+            .dangerous_get_symmetric_key(unwrapped_downstream_key_id)
             .unwrap();
-        assert_eq!(new_encryption_key, unwrapped_encryption_key);
+        assert_eq!(new_downstream_key, unwrapped_downstream_key);
     }
 }
