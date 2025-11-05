3964d25a40e91b2d2b6889f732efba1abf06140b
Nik Gilmore <ngilmore@bitwarden.com>
2025-11-05 12:10:52 -0800

    Test password history in share_cipher

--- crates/bitwarden-vault/src/cipher/cipher_client/share_cipher.rs
+++ crates/bitwarden-vault/src/cipher/cipher_client/share_cipher.rs
@@ -743,4 +743,178 @@ mod tests {
 
         assert!(result.is_err());
     }
+
+    async fn make_test_client_with_wiremock(mock_server: &wiremock::MockServer) -> Client {
+        use bitwarden_core::{
+            ClientSettings, DeviceType, UserId,
+            key_management::crypto::{
+                InitOrgCryptoRequest, InitUserCryptoMethod, InitUserCryptoRequest,
+            },
+        };
+        use bitwarden_crypto::{EncString, Kdf};
+
+        let settings = ClientSettings {
+            identity_url: format!("http://{}", mock_server.address()),
+            api_url: format!("http://{}", mock_server.address()),
+            user_agent: "Bitwarden Test".into(),
+            device_type: DeviceType::SDK,
+            bitwarden_client_version: None,
+        };
+
+        let client = Client::new(Some(settings));
+
+        client
+            .internal
+            .load_flags(std::collections::HashMap::from([(
+                "enableCipherKeyEncryption".to_owned(),
+                true,
+            )]));
+
+        let user_request = InitUserCryptoRequest {
+            user_id: Some(UserId::new(uuid::uuid!("060000fb-0922-4dd3-b170-6e15cb5df8c8"))),
+            kdf_params: Kdf::PBKDF2 {
+                iterations: 600_000.try_into().unwrap(),
+            },
+            email: "test@bitwarden.com".to_owned(),
+            private_key: "2.yN7l00BOlUE0Sb0M//Q53w==|EwKG/BduQRQ33Izqc/ogoBROIoI5dmgrxSo82sgzgAMIBt3A2FZ9vPRMY+GWT85JiqytDitGR3TqwnFUBhKUpRRAq4x7rA6A1arHrFp5Tp1p21O3SfjtvB3quiOKbqWk6ZaU1Np9HwqwAecddFcB0YyBEiRX3VwF2pgpAdiPbSMuvo2qIgyob0CUoC/h4Bz1be7Qa7B0Xw9/fMKkB1LpOm925lzqosyMQM62YpMGkjMsbZz0uPopu32fxzDWSPr+kekNNyLt9InGhTpxLmq1go/pXR2uw5dfpXc5yuta7DB0EGBwnQ8Vl5HPdDooqOTD9I1jE0mRyuBpWTTI3FRnu3JUh3rIyGBJhUmHqGZvw2CKdqHCIrQeQkkEYqOeJRJVdBjhv5KGJifqT3BFRwX/YFJIChAQpebNQKXe/0kPivWokHWwXlDB7S7mBZzhaAPidZvnuIhalE2qmTypDwHy22FyqV58T8MGGMchcASDi/QXI6kcdpJzPXSeU9o+NC68QDlOIrMVxKFeE7w7PvVmAaxEo0YwmuAzzKy9QpdlK0aab/xEi8V4iXj4hGepqAvHkXIQd+r3FNeiLfllkb61p6WTjr5urcmDQMR94/wYoilpG5OlybHdbhsYHvIzYoLrC7fzl630gcO6t4nM24vdB6Ymg9BVpEgKRAxSbE62Tqacxqnz9AcmgItb48NiR/He3n3ydGjPYuKk/ihZMgEwAEZvSlNxYONSbYrIGDtOY+8Nbt6KiH3l06wjZW8tcmFeVlWv+tWotnTY9IqlAfvNVTjtsobqtQnvsiDjdEVtNy/s2ci5TH+NdZluca2OVEr91Wayxh70kpM6ib4UGbfdmGgCo74gtKvKSJU0rTHakQ5L9JlaSDD5FamBRyI0qfL43Ad9qOUZ8DaffDCyuaVyuqk7cz9HwmEmvWU3VQ+5t06n/5kRDXttcw8w+3qClEEdGo1KeENcnXCB32dQe3tDTFpuAIMLqwXs6FhpawfZ5kPYvLPczGWaqftIs/RXJ/EltGc0ugw2dmTLpoQhCqrcKEBDoYVk0LDZKsnzitOGdi9mOWse7Se8798ib1UsHFUjGzISEt6upestxOeupSTOh0v4+AjXbDzRUyogHww3V+Bqg71bkcMxtB+WM+pn1XNbVTyl9NR040nhP7KEf6e9ruXAtmrBC2ah5cFEpLIot77VFZ9ilLuitSz+7T8n1yAh1IEG6xxXxninAZIzi2qGbH69O5RSpOJuJTv17zTLJQIIc781JwQ2TTwTGnx5wZLbffhCasowJKd2EVcyMJyhz6ru0PvXWJ4hUdkARJs3Xu8dus9a86N8Xk6aAPzBDqzYb1vyFIfBxP0oO8xFHgd30Cgmz8UrSE3qeWRrF8ftrI6xQnFjHBGWD/JWSvd6YMcQED0aVuQkuNW9ST/DzQThPzRfPUoiL10yAmV7Ytu4fR3x2sF0Yfi87YhHFuCMpV/DsqxmUizyiJuD938eRcH8hzR/VO53Qo3UIsqOLcyXtTv6THjSlTopQ+JOLOnHm1w8dzYbLN44OG44rRsbihMUQp+wUZ6bsI8rrOnm9WErzkbQFbrfAINdoCiNa6cimYIjvvnMTaFWNymqY1vZxGztQiMiHiHYwTfwHTXrb9j0uPM=|09J28iXv9oWzYtzK2LBT6Yht4IT4MijEkk0fwFdrVQ4=".parse::<EncString>().unwrap(),
+            signing_key: None,
+            security_state: None,
+            method: InitUserCryptoMethod::Password {
+                password: "asdfasdfasdf".to_owned(),
+                user_key: "2.Q/2PhzcC7GdeiMHhWguYAQ==|GpqzVdr0go0ug5cZh1n+uixeBC3oC90CIe0hd/HWA/pTRDZ8ane4fmsEIcuc8eMKUt55Y2q/fbNzsYu41YTZzzsJUSeqVjT8/iTQtgnNdpo=|dwI+uyvZ1h/iZ03VQ+/wrGEFYVewBUUl/syYgjsNMbE=".parse().unwrap(),
+            }
+        };
+
+        let org_request = InitOrgCryptoRequest {
+            organization_keys: std::collections::HashMap::from([(
+                TEST_ORG_ID.parse().unwrap(),
+                "4.rY01mZFXHOsBAg5Fq4gyXuklWfm6mQASm42DJpx05a+e2mmp+P5W6r54WU2hlREX0uoTxyP91bKKwickSPdCQQ58J45LXHdr9t2uzOYyjVzpzebFcdMw1eElR9W2DW8wEk9+mvtWvKwu7yTebzND+46y1nRMoFydi5zPVLSlJEf81qZZ4Uh1UUMLwXz+NRWfixnGXgq2wRq1bH0n3mqDhayiG4LJKgGdDjWXC8W8MMXDYx24SIJrJu9KiNEMprJE+XVF9nQVNijNAjlWBqkDpsfaWTUfeVLRLctfAqW1blsmIv4RQ91PupYJZDNc8nO9ZTF3TEVM+2KHoxzDJrLs2Q==".parse().unwrap()
+            )])
+        };
+
+        client
+            .crypto()
+            .initialize_user_crypto(user_request)
+            .await
+            .unwrap();
+        client
+            .crypto()
+            .initialize_org_crypto(org_request)
+            .await
+            .unwrap();
+
+        client
+    }
+
+    #[tokio::test]
+    async fn test_share_cipher_with_password_history() {
+        use bitwarden_test::start_api_mock;
+        use wiremock::{
+            Mock, ResponseTemplate,
+            matchers::{method, path_regex},
+        };
+        let cipher_id: CipherId = TEST_CIPHER_ID.parse().unwrap();
+        let org_id: OrganizationId = TEST_ORG_ID.parse().unwrap();
+        let collection_id: CollectionId = TEST_COLLECTION_ID_1.parse().unwrap();
+
+        let mut cipher_view = test_cipher_view_without_org();
+        if let Some(ref mut login) = cipher_view.login {
+            login.password = Some("original_password_123".to_string());
+        }
+
+        // Set up wiremock server with mock that echoes back the request data
+        let mock = Mock::given(method("PUT"))
+            .and(path_regex(r"/ciphers/[a-f0-9-]+/share"))
+            .and(wiremock::matchers::body_string_contains("passwordHistory"))
+            .respond_with(move |req: &wiremock::Request| {
+                let body_bytes = req.body.as_slice();
+                let request_body: bitwarden_api_api::models::CipherShareRequestModel =
+                    serde_json::from_slice(body_bytes).expect("Failed to parse request body");
+
+                // Echo back the cipher data
+                let response = CipherResponseModel {
+                    object: Some("cipher".to_string()),
+                    id: Some(cipher_id.into()),
+                    organization_id: Some(
+                        request_body
+                            .cipher
+                            .organization_id
+                            .unwrap()
+                            .parse()
+                            .unwrap(),
+                    ),
+                    r#type: request_body.cipher.r#type,
+                    name: Some(request_body.cipher.name),
+                    notes: request_body.cipher.notes,
+                    login: request_body.cipher.login,
+                    reprompt: request_body.cipher.reprompt,
+                    password_history: request_body.cipher.password_history,
+                    revision_date: Some("2024-01-30T17:55:36.150Z".to_string()),
+                    creation_date: Some("2024-01-30T17:55:36.150Z".to_string()),
+                    edit: Some(true),
+                    view_password: Some(true),
+                    organization_use_totp: Some(true),
+                    favorite: request_body.cipher.favorite,
+                    fields: request_body.cipher.fields,
+                    key: request_body.cipher.key,
+                    ..Default::default()
+                };
+
+                ResponseTemplate::new(200).set_body_json(&response)
+            });
+
+        // Set up the client with mocked server and repository.
+        let (mock_server, _config) = start_api_mock(vec![mock]).await;
+        let client = make_test_client_with_wiremock(&mock_server).await;
+        let repository = std::sync::Arc::new(MemoryRepository::<Cipher>::default());
+        let cipher_client = client.vault().ciphers();
+        let encrypted_original = cipher_client.encrypt(cipher_view.clone()).unwrap();
+        repository
+            .set(
+                TEST_CIPHER_ID.to_string(),
+                encrypted_original.cipher.clone(),
+            )
+            .await
+            .unwrap();
+
+        client
+            .platform()
+            .state()
+            .register_client_managed(repository.clone());
+
+        // Change the password to make sure password_history is updated.
+        if let Some(ref mut login) = cipher_view.login {
+            login.password = Some("new_password_456".to_string());
+        }
+
+        // Act
+        let result = cipher_client
+            .share_cipher(
+                cipher_view.clone(),
+                org_id,
+                vec![collection_id],
+                Some(encrypted_original.cipher),
+            )
+            .await;
+
+        // Assert
+        let shared_cipher = result.unwrap();
+        assert_eq!(shared_cipher.organization_id, Some(org_id));
+        let decrypted_view = cipher_client.decrypt(shared_cipher.clone()).unwrap();
+        let history = decrypted_view.password_history.unwrap();
+        assert_eq!(
+            history.len(),
+            1,
+            "Password history should have 1 entry for the changed password"
+        );
+        assert_eq!(
+            history[0].password, "original_password_123",
+            "Password history should contain the original password"
+        );
+        assert_eq!(
+            decrypted_view.login.as_ref().unwrap().password,
+            Some("new_password_456".to_string()),
+            "New password should be set"
+        );
+    }
 }
