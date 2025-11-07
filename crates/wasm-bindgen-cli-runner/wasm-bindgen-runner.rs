74e13e1128973caf3e1b5ec5ab52728e72d27ed3
Daniel García <dani-garcia@users.noreply.github.com>
2025-11-07 15:40:03 +0100

    Add comment and match version definition

--- crates/wasm-bindgen-cli-runner/wasm-bindgen-runner.rs
+++ crates/wasm-bindgen-cli-runner/wasm-bindgen-runner.rs
@@ -1,3 +1,6 @@
+//! Wrapper binary for `wasm-bindgen` CLI to ensure version consistency and avoid manual
+//! installation.
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     wasm_bindgen_cli::wasm_bindgen::run_cli_with_args(std::env::args_os())?;
