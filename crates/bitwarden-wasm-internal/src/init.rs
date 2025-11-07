0430b51324087b75de023a35e011203678a0b4a0
neuronull <9162534+neuronull@users.noreply.github.com>
2025-11-07 14:30:26 -0700

    .

--- crates/bitwarden-wasm-internal/src/init.rs
+++ crates/bitwarden-wasm-internal/src/init.rs
@@ -1,4 +1,11 @@
-use log::{Level, set_max_level};
+use tracing::Level;
+use tracing_subscriber::{
+    EnvFilter,
+    fmt::{format::Pretty, time::UtcTime},
+    layer::SubscriberExt as _,
+    util::SubscriberInitExt as _,
+};
+use tracing_web::{MakeWebConsoleWriter, performance_layer};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -12,26 +19,35 @@ pub enum LogLevel {
 
 fn convert_level(level: LogLevel) -> Level {
     match level {
-        LogLevel::Trace => Level::Trace,
-        LogLevel::Debug => Level::Debug,
-        LogLevel::Info => Level::Info,
-        LogLevel::Warn => Level::Warn,
-        LogLevel::Error => Level::Error,
+        LogLevel::Trace => Level::TRACE,
+        LogLevel::Debug => Level::DEBUG,
+        LogLevel::Info => Level::INFO,
+        LogLevel::Warn => Level::WARN,
+        LogLevel::Error => Level::ERROR,
     }
-}
-
-#[wasm_bindgen]
-pub fn set_log_level(level: LogLevel) {
-    let log_level = convert_level(level);
-    set_max_level(log_level.to_level_filter());
 }
 
 #[allow(missing_docs)]
 #[wasm_bindgen]
 pub fn init_sdk(log_level: Option<LogLevel>) {
     console_error_panic_hook::set_once();
+
     let log_level = convert_level(log_level.unwrap_or(LogLevel::Info));
-    if let Err(_e) = console_log::init_with_level(log_level) {
-        set_max_level(log_level.to_level_filter())
-    }
+
+    let filter = EnvFilter::builder()
+        .with_default_directive(log_level.into())
+        .from_env_lossy();
+
+    let fmt = tracing_subscriber::fmt::layer()
+        .with_ansi(false) // Only partially supported across browsers
+        .with_timer(UtcTime::rfc_3339())
+        .with_writer(MakeWebConsoleWriter::new()); // write events to the console
+
+    let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
+
+    tracing_subscriber::registry()
+        .with(perf_layer)
+        .with(filter)
+        .with(fmt)
+        .init();
 }
