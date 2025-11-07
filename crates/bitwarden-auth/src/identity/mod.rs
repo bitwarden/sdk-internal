4991a67e358181d29d1a45c3252995415eac7d3f
Jared Snider <jsnider@bitwarden.com>
2025-11-07 16:20:36 -0500

    PM-14922 - (1) create password_login feature folder (2) create password_prelogin file to prevent identity client file from growing substantially as we add items (per feedback from platform)

--- crates/bitwarden-auth/src/identity/mod.rs
+++ crates/bitwarden-auth/src/identity/mod.rs
@@ -1,5 +1,8 @@
 //! Identity client module
 //! The IdentityClient is used to obtain identity / access tokens from the Bitwarden Identity API.
 mod client;
+/// Password-based authentication functionality
+mod password_login;
 
-pub use client::{IdentityClient, PasswordPreloginData, PasswordPreloginError};
+pub use client::IdentityClient;
+pub use password_login::{PasswordPreloginData, PasswordPreloginError};
---
+++ crates/bitwarden-auth/src/identity/password_login/mod.rs
@@ -0,0 +1,3 @@
+mod password_prelogin;
+
+pub use password_prelogin::{PasswordPreloginData, PasswordPreloginError};
