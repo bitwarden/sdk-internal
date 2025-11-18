52ac0688a4ae3cda82bf95bdf7e4c3ea6b86d580
Thomas Avery <tavery@bitwarden.com>
2025-11-18 09:43:38 -0600

    Add back comment and update to correct method.

--- crates/bitwarden-core/src/key_management/crypto.rs
+++ crates/bitwarden-core/src/key_management/crypto.rs
@@ -360,6 +360,7 @@ pub(super) fn make_update_kdf(
     })
 }
 
+/// Response from the `make_update_password` function
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 #[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
