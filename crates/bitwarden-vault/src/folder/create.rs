f031cab55a602279da57c0c05e17b36a6802df44
Bernd Schoolmann <mail@quexten.com>
2025-11-21 15:08:37 +0100

    Run cargo fmt

--- crates/bitwarden-vault/src/folder/create.rs
+++ crates/bitwarden-vault/src/folder/create.rs
@@ -99,10 +99,8 @@ mod tests {
     async fn test_create_folder() {
         let store: KeyStore<KeyIds> = KeyStore::default();
         let mut ctx = store.context_mut();
-        let local_key_id = ctx
-            .make_symmetric_key(SymmetricKeyAlgorithm::Aes256CbcHmac);
-        ctx
-            .persist_symmetric_key(local_key_id, SymmetricKeyId::User)
+        let local_key_id = ctx.make_symmetric_key(SymmetricKeyAlgorithm::Aes256CbcHmac);
+        ctx.persist_symmetric_key(local_key_id, SymmetricKeyId::User)
             .unwrap();
         drop(ctx);
 
@@ -163,10 +161,8 @@ mod tests {
     async fn test_create_folder_http_error() {
         let store: KeyStore<KeyIds> = KeyStore::default();
         let mut ctx = store.context_mut();
-        let local_key_id = ctx
-            .make_symmetric_key(SymmetricKeyAlgorithm::Aes256CbcHmac);
-        ctx
-            .persist_symmetric_key(local_key_id, SymmetricKeyId::User)
+        let local_key_id = ctx.make_symmetric_key(SymmetricKeyAlgorithm::Aes256CbcHmac);
+        ctx.persist_symmetric_key(local_key_id, SymmetricKeyId::User)
             .unwrap();
         drop(ctx);
 
