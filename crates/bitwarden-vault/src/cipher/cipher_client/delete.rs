f3a06f6b6076b09e17717cba769423bcd8fb6196
Nik Gilmore <ngilmore@bitwarden.com>
2025-11-24 13:39:19 -0800

    Add separate delete_as_admin functions

--- crates/bitwarden-vault/src/cipher/cipher_client/delete.rs
+++ crates/bitwarden-vault/src/cipher/cipher_client/delete.rs
@@ -27,15 +27,35 @@ impl<T> From<bitwarden_api_api::apis::Error<T>> for RestoreCipherError {
 }
 
 impl CiphersClient {
-    /// Deletes the [Cipher] with the matching [CipherId] from the server.
-    pub async fn delete(&self, cipher_id: CipherId, as_admin: bool) -> Result<(), ApiError> {
+    /// Deletes the [Cipher] with the matching [CipherId] from the server, using the admin endpoint.
+    pub async fn delete_as_admin(&self, cipher_id: CipherId) -> Result<(), ApiError> {
         let configs = self.get_api_configurations().await;
         let api = configs.api_client.ciphers_api();
-        if as_admin {
-            api.delete_admin(cipher_id.into()).await?
-        } else {
-            api.delete(cipher_id.into()).await?
-        };
+        api.delete_admin(cipher_id.into()).await?;
+        Ok(())
+    }
+
+    /// Deletes the [Cipher] with the matching [CipherId] from the server.
+    pub async fn delete(&self, cipher_id: CipherId) -> Result<(), ApiError> {
+        let configs = self.get_api_configurations().await;
+        let api = configs.api_client.ciphers_api();
+        api.delete(cipher_id.into()).await?;
+        Ok(())
+    }
+
+    /// Deletes all [Cipher] objects with a matching [CipherId] from the server, using the admin endpoint.
+    pub async fn delete_many_as_admin(
+        &self,
+        cipher_ids: Vec<CipherId>,
+        organization_id: Option<OrganizationId>,
+    ) -> Result<(), ApiError> {
+        let configs = self.get_api_configurations().await;
+        let api = configs.api_client.ciphers_api();
+        api.delete_many_admin(Some(CipherBulkDeleteRequestModel {
+            ids: cipher_ids.into_iter().map(|id| id.to_string()).collect(),
+            organization_id: organization_id.map(|id| id.to_string()),
+        }))
+        .await?;
         Ok(())
     }
 
@@ -44,35 +64,30 @@ impl CiphersClient {
         &self,
         cipher_ids: Vec<CipherId>,
         organization_id: Option<OrganizationId>,
-        as_admin: bool,
     ) -> Result<(), ApiError> {
         let configs = self.get_api_configurations().await;
         let api = configs.api_client.ciphers_api();
-        if as_admin {
-            api.delete_many_admin(Some(CipherBulkDeleteRequestModel {
-                ids: cipher_ids.into_iter().map(|id| id.to_string()).collect(),
-                organization_id: organization_id.map(|id| id.to_string()),
-            }))
-            .await?;
-        } else {
-            api.delete_many(Some(CipherBulkDeleteRequestModel {
-                ids: cipher_ids.into_iter().map(|id| id.to_string()).collect(),
-                organization_id: organization_id.map(|id| id.to_string()),
-            }))
-            .await?;
-        };
+        api.delete_many(Some(CipherBulkDeleteRequestModel {
+            ids: cipher_ids.into_iter().map(|id| id.to_string()).collect(),
+            organization_id: organization_id.map(|id| id.to_string()),
+        }))
+        .await?;
         Ok(())
     }
 
     /// Soft-deletes the [Cipher] with the matching [CipherId] from the server.
-    pub async fn soft_delete(&self, cipher_id: CipherId, as_admin: bool) -> Result<(), ApiError> {
+    pub async fn soft_delete(&self, cipher_id: CipherId) -> Result<(), ApiError> {
         let configs = self.get_api_configurations().await;
         let api = configs.api_client.ciphers_api();
-        if as_admin {
-            api.put_delete_admin(cipher_id.into()).await?; // TODO: Map errors properly.
-        } else {
-            api.put_delete(cipher_id.into()).await?;
-        };
+        api.put_delete(cipher_id.into()).await?;
+        Ok(())
+    }
+
+    /// Soft-deletes the [Cipher] with the matching [CipherId] from the server, using the admin endpoint.
+    pub async fn soft_delete_as_admin(&self, cipher_id: CipherId) -> Result<(), ApiError> {
+        let configs = self.get_api_configurations().await;
+        let api = configs.api_client.ciphers_api();
+        api.put_delete_admin(cipher_id.into()).await?; // TODO: Map errors properly.
         Ok(())
     }
 
@@ -81,44 +96,52 @@ impl CiphersClient {
         &self,
         cipher_ids: Vec<CipherId>,
         organization_id: Option<OrganizationId>,
-        as_admin: bool,
     ) -> Result<(), ApiError> {
         let configs = self.get_api_configurations().await;
         let api = configs.api_client.ciphers_api();
-        if as_admin {
-            api.put_delete_many_admin(Some(CipherBulkDeleteRequestModel {
-                ids: cipher_ids.into_iter().map(|id| id.to_string()).collect(),
-                organization_id: organization_id.map(|id| id.to_string()),
-            }))
-            .await?;
-        } else {
-            api.put_delete_many(Some(CipherBulkDeleteRequestModel {
-                ids: cipher_ids.into_iter().map(|id| id.to_string()).collect(),
-                organization_id: organization_id.map(|id| id.to_string()),
-            }))
-            .await?
-        };
+        api.put_delete_many(Some(CipherBulkDeleteRequestModel {
+            ids: cipher_ids.into_iter().map(|id| id.to_string()).collect(),
+            organization_id: organization_id.map(|id| id.to_string()),
+        }))
+        .await?;
+        Ok(())
+    }
+
+    /// Soft-deletes all [Cipher] objects for the given [CipherId]s from the server, using the admin endpoint.
+    pub async fn soft_delete_many_as_admin(
+        &self,
+        cipher_ids: Vec<CipherId>,
+        organization_id: Option<OrganizationId>,
+    ) -> Result<(), ApiError> {
+        let configs = self.get_api_configurations().await;
+        let api = configs.api_client.ciphers_api();
+        api.put_delete_many_admin(Some(CipherBulkDeleteRequestModel {
+            ids: cipher_ids.into_iter().map(|id| id.to_string()).collect(),
+            organization_id: organization_id.map(|id| id.to_string()),
+        }))
+        .await?;
         Ok(())
     }
 
     /// Restores a soft-deleted cipher on the server.
-    pub async fn restore(
+    pub async fn restore(&self, cipher_id: CipherId) -> Result<CipherView, RestoreCipherError> {
+        let api_config = self.get_api_configurations().await;
+        let api = api_config.api_client.ciphers_api();
+
+        let cipher: Cipher = api.put_restore(cipher_id.into()).await?.try_into()?;
+
+        Ok(self.decrypt(cipher)?)
+    }
+
+    /// Restores a soft-deleted cipher on the server, using the admin endpoint.
+    pub async fn restore_as_admin(
         &self,
         cipher_id: CipherId,
-        is_admin: bool,
     ) -> Result<CipherView, RestoreCipherError> {
         let api_config = self.get_api_configurations().await;
         let api = api_config.api_client.ciphers_api();
 
-        let cipher: Cipher = if is_admin {
-            let response = api.put_restore_admin(cipher_id.into()).await?;
-            response.try_into()?
-        } else {
-            let response = api.put_restore(cipher_id.into()).await?;
-            let cipher: Cipher = response.try_into()?;
-
-            cipher
-        };
+        let cipher: Cipher = api.put_restore_admin(cipher_id.into()).await?.try_into()?;
 
         Ok(self.decrypt(cipher)?)
     }
