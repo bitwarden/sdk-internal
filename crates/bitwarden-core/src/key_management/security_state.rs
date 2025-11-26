1bdd7130f7251cbf568097e7a61fc6b452a61bbc
Bernd Schoolmann <mail@quexten.com>
2025-11-26 14:12:16 +0100

    Re-use from impl

--- crates/bitwarden-core/src/key_management/security_state.rs
+++ crates/bitwarden-core/src/key_management/security_state.rs
@@ -116,8 +116,7 @@ impl TryFrom<&CoseSign1Bytes> for SignedSecurityState {
 
 impl From<&SignedSecurityState> for String {
     fn from(val: &SignedSecurityState) -> Self {
-        let bytes: CoseSign1Bytes = val.clone().into();
-        B64::from(bytes.as_ref()).to_string()
+        val.clone().into()
     }
 }
 
