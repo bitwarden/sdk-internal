94202af16fe6edb626d47511d4bf437a2747b2ee
Bernd Schoolmann <mail@quexten.com>
2025-12-16 13:33:22 +0100

    Remove unused wasm macro derive

--- crates/bitwarden-core/src/error.rs
+++ crates/bitwarden-core/src/error.rs
@@ -53,11 +53,6 @@ pub struct NotAuthenticatedError;
 
 /// Client's user ID is already set.
 #[derive(Debug, Error, serde::Serialize, serde::Deserialize, Clone)]
-#[cfg_attr(
-    feature = "wasm",
-    derive(tsify::Tsify),
-    tsify(into_wasm_abi, from_wasm_abi)
-)]
 #[error("The client user ID is already set")]
 pub struct UserIdAlreadySetError;
 
