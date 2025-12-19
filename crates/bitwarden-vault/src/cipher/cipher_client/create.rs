bd6014093cde9cf99f6d08370c4ae4d2c7257887
Nik Gilmore <ngilmore@bitwarden.com>
2025-12-19 15:48:12 -0800

    REmove wiremock from test_create_org_cipher

--- crates/bitwarden-vault/src/cipher/cipher_client/create.rs
+++ crates/bitwarden-vault/src/cipher/cipher_client/create.rs
@@ -311,23 +311,12 @@ impl CiphersClient {
 #[cfg(test)]
 mod tests {
     use bitwarden_api_api::{apis::ApiClient, models::CipherResponseModel};
-    use bitwarden_core::{
-        Client, ClientSettings, DeviceType, UserId,
-        key_management::{
-            account_cryptographic_state::WrappedAccountCryptographicState,
-            crypto::{InitOrgCryptoRequest, InitUserCryptoMethod, InitUserCryptoRequest},
-        },
-    };
-    use bitwarden_crypto::{EncString, Kdf, SymmetricKeyAlgorithm};
-    use bitwarden_test::{MemoryRepository, start_api_mock};
+    use bitwarden_crypto::SymmetricKeyAlgorithm;
+    use bitwarden_test::MemoryRepository;
     use chrono::Utc;
-    use wiremock::{
-        Mock, ResponseTemplate,
-        matchers::{method, path},
-    };
 
     use super::*;
-    use crate::{CipherId, LoginView, VaultClientExt};
+    use crate::{CipherId, LoginView};
 
     const TEST_CIPHER_ID: &str = "5faa9684-c793-4a2d-8a12-b33900187097";
     const TEST_COLLECTION_ID: &str = "73546b86-8802-4449-ad2a-69ea981b4ffd";
@@ -354,65 +343,6 @@ mod tests {
             fields: Default::default(),
             collection_ids: vec![],
         }
-    }
-
-    async fn create_client_with_wiremock(mock_server: &wiremock::MockServer) -> CiphersClient {
-        let settings = ClientSettings {
-            identity_url: format!("http://{}", mock_server.address()),
-            api_url: format!("http://{}", mock_server.address()),
-            user_agent: "Bitwarden Test".into(),
-            device_type: DeviceType::SDK,
-            bitwarden_client_version: None,
-        };
-
-        let client = Client::new(Some(settings));
-
-        client
-            .internal
-            .load_flags(std::collections::HashMap::from([(
-                "enableCipherKeyEncryption".to_owned(),
-                true,
-            )]));
-
-        let user_request = InitUserCryptoRequest {
-            user_id: Some(UserId::new(uuid::uuid!(TEST_USER_ID))),
-            kdf_params: Kdf::PBKDF2 {
-                iterations: 600_000.try_into().unwrap(),
-            },
-            email: "test@bitwarden.com".to_owned(),
-            method: InitUserCryptoMethod::Password {
-                password: "asdfasdfasdf".to_owned(),
-                user_key: "2.Q/2PhzcC7GdeiMHhWguYAQ==|GpqzVdr0go0ug5cZh1n+uixeBC3oC90CIe0hd/HWA/pTRDZ8ane4fmsEIcuc8eMKUt55Y2q/fbNzsYu41YTZzzsJUSeqVjT8/iTQtgnNdpo=|dwI+uyvZ1h/iZ03VQ+/wrGEFYVewBUUl/syYgjsNMbE=".parse().unwrap(),
-            },
-            account_cryptographic_state: WrappedAccountCryptographicState::V1 {
-                private_key: "2.yN7l00BOlUE0Sb0M//Q53w==|EwKG/BduQRQ33Izqc/ogoBROIoI5dmgrxSo82sgzgAMIBt3A2FZ9vPRMY+GWT85JiqytDitGR3TqwnFUBhKUpRRAq4x7rA6A1arHrFp5Tp1p21O3SfjtvB3quiOKbqWk6ZaU1Np9HwqwAecddFcB0YyBEiRX3VwF2pgpAdiPbSMuvo2qIgyob0CUoC/h4Bz1be7Qa7B0Xw9/fMKkB1LpOm925lzqosyMQM62YpMGkjMsbZz0uPopu32fxzDWSPr+kekNNyLt9InGhTpxLmq1go/pXR2uw5dfpXc5yuta7DB0EGBwnQ8Vl5HPdDooqOTD9I1jE0mRyuBpWTTI3FRnu3JUh3rIyGBJhUmHqGZvw2CKdqHCIrQeQkkEYqOeJRJVdBjhv5KGJifqT3BFRwX/YFJIChAQpebNQKXe/0kPivWokHWwXlDB7S7mBZzhaAPidZvnuIhalE2qmTypDwHy22FyqV58T8MGGMchcASDi/QXI6kcdpJzPXSeU9o+NC68QDlOIrMVxKFeE7w7PvVmAaxEo0YwmuAzzKy9QpdlK0aab/xEi8V4iXj4hGepqAvHkXIQd+r3FNeiLfllkb61p6WTjr5urcmDQMR94/wYoilpG5OlybHdbhsYHvIzYoLrC7fzl630gcO6t4nM24vdB6Ymg9BVpEgKRAxSbE62Tqacxqnz9AcmgItb48NiR/He3n3ydGjPYuKk/ihZMgEwAEZvSlNxYONSbYrIGDtOY+8Nbt6KiH3l06wjZW8tcmFeVlWv+tWotnTY9IqlAfvNVTjtsobqtQnvsiDjdEVtNy/s2ci5TH+NdZluca2OVEr91Wayxh70kpM6ib4UGbfdmGgCo74gtKvKSJU0rTHakQ5L9JlaSDD5FamBRyI0qfL43Ad9qOUZ8DaffDCyuaVyuqk7cz9HwmEmvWU3VQ+5t06n/5kRDXttcw8w+3qClEEdGo1KeENcnXCB32dQe3tDTFpuAIMLqwXs6FhpawfZ5kPYvLPczGWaqftIs/RXJ/EltGc0ugw2dmTLpoQhCqrcKEBDoYVk0LDZKsnzitOGdi9mOWse7Se8798ib1UsHFUjGzISEt6upestxOeupSTOh0v4+AjXbDzRUyogHww3V+Bqg71bkcMxtB+WM+pn1XNbVTyl9NR040nhP7KEf6e9ruXAtmrBC2ah5cFEpLIot77VFZ9ilLuitSz+7T8n1yAh1IEG6xxXxninAZIzi2qGbH69O5RSpOJuJTv17zTLJQIIc781JwQ2TTwTGnx5wZLbffhCasowJKd2EVcyMJyhz6ru0PvXWJ4hUdkARJs3Xu8dus9a86N8Xk6aAPzBDqzYb1vyFIfBxP0oO8xFHgd30Cgmz8UrSE3qeWRrF8ftrI6xQnFjHBGWD/JWSvd6YMcQED0aVuQkuNW9ST/DzQThPzRfPUoiL10yAmV7Ytu4fR3x2sF0Yfi87YhHFuCMpV/DsqxmUizyiJuD938eRcH8hzR/VO53Qo3UIsqOLcyXtTv6THjSlTopQ+JOLOnHm1w8dzYbLN44OG44rRsbihMUQp+wUZ6bsI8rrOnm9WErzkbQFbrfAINdoCiNa6cimYIjvvnMTaFWNymqY1vZxGztQiMiHiHYwTfwHTXrb9j0uPM=|09J28iXv9oWzYtzK2LBT6Yht4IT4MijEkk0fwFdrVQ4=".parse::<EncString>().unwrap(),
-            }
-        };
-
-        let org_request = InitOrgCryptoRequest {
-            organization_keys: std::collections::HashMap::from([(
-                TEST_ORG_ID.parse().unwrap(),
-                "4.rY01mZFXHOsBAg5Fq4gyXuklWfm6mQASm42DJpx05a+e2mmp+P5W6r54WU2hlREX0uoTxyP91bKKwickSPdCQQ58J45LXHdr9t2uzOYyjVzpzebFcdMw1eElR9W2DW8wEk9+mvtWvKwu7yTebzND+46y1nRMoFydi5zPVLSlJEf81qZZ4Uh1UUMLwXz+NRWfixnGXgq2wRq1bH0n3mqDhayiG4LJKgGdDjWXC8W8MMXDYx24SIJrJu9KiNEMprJE+XVF9nQVNijNAjlWBqkDpsfaWTUfeVLRLctfAqW1blsmIv4RQ91PupYJZDNc8nO9ZTF3TEVM+2KHoxzDJrLs2Q==".parse().unwrap()
-            )])
-        };
-
-        client
-            .crypto()
-            .initialize_user_crypto(user_request)
-            .await
-            .unwrap();
-        client
-            .crypto()
-            .initialize_org_crypto(org_request)
-            .await
-            .unwrap();
-
-        client
-            .platform()
-            .state()
-            .register_client_managed(std::sync::Arc::new(MemoryRepository::<Cipher>::default()));
-
-        client.vault().ciphers()
     }
 
     #[tokio::test]
@@ -553,15 +483,13 @@ mod tests {
 
     #[tokio::test]
     async fn test_create_org_cipher() {
-        let (mock_server, _config) = start_api_mock(vec![
-            Mock::given(method("POST"))
-                .and(path(r"/ciphers/create"))
-                .respond_with(move |req: &wiremock::Request| {
-                    let body_bytes = req.body.as_slice();
-                    let request_body: CipherCreateRequestModel =
-                        serde_json::from_slice(body_bytes).expect("Failed to parse request body");
+        let api_client = ApiClient::new_mocked(move |mock| {
+            mock.ciphers_api
+                .expect_post_create()
+                .returning(move |body| {
+                    let request_body = body.unwrap();
 
-                    let response = CipherResponseModel {
+                    Ok(CipherResponseModel {
                         id: Some(TEST_CIPHER_ID.try_into().unwrap()),
                         organization_id: request_body
                             .cipher
@@ -572,44 +500,58 @@ mod tests {
                         creation_date: Some(Utc::now().to_string()),
                         revision_date: Some(Utc::now().to_string()),
                         ..Default::default()
-                    };
+                    })
+                })
+                .once();
+        });
 
-                    ResponseTemplate::new(200).set_body_json(&response)
-                }),
-        ])
-        .await;
-
-        let client = create_client_with_wiremock(&mock_server).await;
-        let response = client
-            .create_cipher(CipherCreateRequest {
-                organization_id: Some(TEST_ORG_ID.parse().unwrap()),
-                collection_ids: vec![TEST_COLLECTION_ID.parse().unwrap()],
-                folder_id: None,
-                name: "Test Cipher".into(),
-                notes: None,
-                favorite: false,
-                reprompt: CipherRepromptType::None,
-                r#type: CipherViewType::Login(LoginView {
-                    username: None,
-                    password: None,
-                    password_revision_date: None,
-                    uris: None,
-                    totp: None,
-                    autofill_on_page_load: None,
-                    fido2_credentials: None,
-                }),
-                fields: vec![],
-            })
-            .await
+        let store: KeyStore<KeyIds> = KeyStore::default();
+        {
+            let mut ctx = store.context_mut();
+            let local_key_id = ctx.make_symmetric_key(SymmetricKeyAlgorithm::Aes256CbcHmac);
+            ctx.persist_symmetric_key(
+                local_key_id,
+                SymmetricKeyId::Organization(TEST_ORG_ID.parse().unwrap()),
+            )
             .unwrap();
+        }
+        let repository = MemoryRepository::<Cipher>::default();
+        let request = CipherCreateRequest {
+            organization_id: Some(TEST_ORG_ID.parse().unwrap()),
+            collection_ids: vec![TEST_COLLECTION_ID.parse().unwrap()],
+            folder_id: None,
+            name: "Test Cipher".into(),
+            notes: None,
+            favorite: false,
+            reprompt: CipherRepromptType::None,
+            r#type: CipherViewType::Login(LoginView {
+                username: None,
+                password: None,
+                password_revision_date: None,
+                uris: None,
+                totp: None,
+                autofill_on_page_load: None,
+                fido2_credentials: None,
+            }),
+            fields: vec![],
+        };
 
-        let repository = client.get_repository().unwrap();
+        let response = create_cipher(
+            &store,
+            &api_client,
+            &repository,
+            TEST_USER_ID.parse().unwrap(),
+            request.into(),
+        )
+        .await
+        .unwrap();
+
         let cipher: Cipher = repository
             .get(TEST_CIPHER_ID.to_string())
             .await
             .unwrap()
             .unwrap();
-        let cipher_view = client.decrypt(cipher).unwrap();
+        let cipher_view: CipherView = store.decrypt(&cipher).unwrap();
 
         assert_eq!(response.id, cipher_view.id);
         assert_eq!(response.organization_id, cipher_view.organization_id);
