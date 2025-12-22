57bb9831de92ecefdbc4a11be5b6510673b37aac
Jared Snider <jsnider@bitwarden.com>
2025-12-22 17:21:25 -0500

    PM-14922 - LoginSuccessApiResponse - update docs on private key prop

--- crates/bitwarden-auth/src/identity/api/response/login_success_api_response.rs
+++ crates/bitwarden-auth/src/identity/api/response/login_success_api_response.rs
@@ -30,8 +30,8 @@ pub(crate) struct LoginSuccessApiResponse {
     // we add aliases for deserialization flexibility.
     /// The user key wrapped user private key
     /// Deprecated in favor of the `AccountKeys` field but still present for backward compatibility.
-    /// and we can't expose AccountKeys in our LoginSuccessResponse until we get an SDK
-    /// response model with WASM / uniffi support.
+    /// and we can't expose AccountKeys in our LoginSuccessResponse until we get a PrivateKeysResponseModel
+    /// SDK response model from KM with WASM / uniffi support.
     #[serde(rename = "PrivateKey", alias = "privateKey")]
     pub private_key: Option<String>,
 
