f8a0f89df225a873a1ba81af84134f87c31960e9
Bernd Schoolmann <mail@quexten.com>
2026-01-16 14:46:23 +0100

    Fix build

--- crates/bitwarden-vault/src/folder/folder_models.rs
+++ crates/bitwarden-vault/src/folder/folder_models.rs
@@ -28,15 +28,6 @@ pub struct Folder {
     pub revision_date: DateTime<Utc>,
 }
 
-impl From<&Folder> for FolderWithIdRequestModel {
-    fn from(val: &Folder) -> Self {
-        FolderWithIdRequestModel {
-            name: val.name.to_string(),
-            id: val.id.map(|id| id.0),
-        }
-    }
-}
-
 bitwarden_state::register_repository_item!(Folder, "Folder");
 
 #[allow(missing_docs)]
