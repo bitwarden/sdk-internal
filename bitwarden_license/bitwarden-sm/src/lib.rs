6d0bab5b8f96fd8e3db05cb27fe65d602049d489
Daniel García <dani-garcia@users.noreply.github.com>
2026-02-01 20:01:41 +0100

    Wrap SM code in a client

--- bitwarden_license/bitwarden-sm/src/lib.rs
+++ bitwarden_license/bitwarden-sm/src/lib.rs
@@ -1,5 +1,6 @@
 #![doc = include_str!("../README.md")]
 
+pub mod client;
 mod client_projects;
 mod client_secrets;
 mod error;
@@ -8,5 +9,10 @@ pub mod projects;
 #[allow(missing_docs)]
 pub mod secrets;
 
-pub use client_projects::{ClientProjects, ClientProjectsExt, ProjectsClient, ProjectsClientExt};
-pub use client_secrets::{ClientSecrets, ClientSecretsExt, SecretsClient, SecretsClientExt};
+pub use bitwarden_core::auth::{
+    AccessToken,
+    login::{AccessTokenLoginRequest, AccessTokenLoginResponse},
+};
+pub use client::{ClientSettings, SecretsManagerClient};
+pub use client_projects::ProjectsClient;
+pub use client_secrets::SecretsClient;
