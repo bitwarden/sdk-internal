15d59c378baa57fb3f7a35c8cd239f09d9b38d18
Bernd Schoolmann <accounts@quexten.com>
2026-02-02 15:13:22 +0100

    [PM-30144] Add user crypto management client  (#687)
    ## 🎟️ Tracking
    
    <!-- Paste the link to the Jira or GitHub issue or otherwise describe /
    point to where this change is coming from. -->
    https://bitwarden.atlassian.net/browse/PM-30144
    
    ## 📔 Objective
    
    <!-- Describe what the purpose of this PR is, for example what bug
    you're fixing or new feature you're adding. -->
    Adds the scaffolding for the user crypto management client. This client
    is intended to be used for managing a user's crypto (upgrading crypto
    state, setting kdf settings, etc), but not implementing the
    cryptographic operations itself.
    
    
    ## 🚨 Breaking Changes
    
    <!-- Does this PR introduce any breaking changes? If so, please describe
    the impact and migration path for clients.
    
    If you're unsure, the automated TypeScript compatibility check will run
    when you open/update this PR and provide feedback.
    
    For breaking changes:
    1. Describe what changed in the client interface
    2. Explain why the change was necessary
    3. Provide migration steps for client developers
    4. Link to any paired client PRs if needed
    
    Otherwise, you can remove this section. -->
    
    ## ⏰ Reminders before review
    
    - Contributor guidelines followed
    - All formatters and local linters executed and passed
    - Written new unit and / or integration tests where applicable
    - Protected functional changes with optionality (feature flags)
    - Used internationalization (i18n) for all UI strings
    - CI builds passed
    - Communicated to DevOps any deployment requirements
    - Updated any necessary documentation (Confluence, contributing docs) or
    informed the documentation
      team
    
    ## 🦮 Reviewer guidelines
    
    <!-- Suggested interactions but feel free to use (or not) as you desire!
    -->
    
    - 👍 (`:+1:`) or similar for great changes
    - 📝 (`:memo:`) or ℹ️ (`:information_source:`) for notes or general info
    - ❓ (`:question:`) for questions
    - 🤔 (`:thinking:`) or 💭 (`:thought_balloon:`) for more open inquiry
    that's not quite a confirmed
      issue and could potentially benefit from discussion
    - 🎨 (`:art:`) for suggestions / improvements
    - ❌ (`:x:`) or ⚠️ (`:warning:`) for more significant problems or
    concerns needing attention
    - 🌱 (`:seedling:`) or ♻️ (`:recycle:`) for future improvements or
    indications of technical debt
    - ⛏ (`:pick:`) for minor or nitpick changes

---
+++ crates/bitwarden-user-crypto-management/README.md
@@ -0,0 +1,7 @@
+# Bitwarden User Crypto Management
+
+Contains operations to manage a user's keys, decryption methods and data.
+
+## User Key Rotation
+
+Allows changing the user's symmetric userkey, and re-encrypting the data protected by it.
---
+++ crates/bitwarden-user-crypto-management/src/lib.rs
@@ -0,0 +1,10 @@
+#![doc = include_str!("../README.md")]
+
+// Enable uniffi scaffolding when the "uniffi" feature is enabled.
+#[cfg(feature = "uniffi")]
+uniffi::setup_scaffolding!();
+
+mod user_crypto_management_client;
+pub use user_crypto_management_client::{
+    UserCryptoManagementClient, UserCryptoManagementClientExt,
+};
