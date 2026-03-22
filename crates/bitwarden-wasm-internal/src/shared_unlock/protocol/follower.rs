6117d5f4263fac186d80fb062c596aba70c2d8a3
Bernd Schoolmann <mail@quexten.com>
2026-03-22 12:39:39 +0900

    Start session

--- crates/bitwarden-wasm-internal/src/shared_unlock/protocol/follower.rs
+++ crates/bitwarden-wasm-internal/src/shared_unlock/protocol/follower.rs
@@ -25,6 +25,17 @@ impl<L: UserLockManagement, S: MessageSender, D: LeaderDiscovery, H: HeartbeatRe
         heartbeat_response_handler: H,
         sender: S,
     ) -> Self {
+        let users: Vec<bitwarden_core::UserId> = lock_system.list_users().await;
+        let leader = leader_discovery.discover_leader().await.unwrap();
+        for user_id in users {
+            let lock_state = lock_system.get_user_lock_state(user_id).await;
+            let message = Message::StartSession {
+                user_id,
+                lock_state,
+            };
+            sender.send_message(message, leader);
+        }
+
         Self {
             lock_system,
             leader_discovery,
