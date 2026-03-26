5f0e46661baa669e5c5d3f669d7683711959796d
Bernd Schoolmann <mail@quexten.com>
2026-03-26 09:54:08 +0900

    Add security defintions

--- crates/bitwarden-ipc/src/crypto_provider/noise/mod.rs
+++ crates/bitwarden-ipc/src/crypto_provider/noise/mod.rs
@@ -3,6 +3,18 @@
 //! Because of this, it is accepted that a new handshake is done. Security is provided
 //! against passive attackers both of the traffic, and subsequent key compromise, but
 //! active attacks are not protected against.
+//! 
+//! Security Definition SD1:
+//! Attacker Model:
+//! - Attacker has full passive read access to the entire IPC conversation
+//! Security Goal:
+//! - Attacker should not be able to derive any information about the plaintext messages beyond length and timing.
+//! 
+//! Security Definition SD2:
+//! Attacker Model:
+//! - Attacker has full passive read access to the entire IPC conversation and access to the session state of one side at time X
+//! Security Goal:
+//! - Attacker should only be able to decrypt messages that were received or sent within the re-handshake interval
 
 // Ref: http://noiseprotocol.org/noise.html#message-format
 const NOISE_MAX_MESSAGE_LEN: usize = 65535;
