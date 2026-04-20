9b143dec13e0980a40b41fcee1398c0f4c506490
JaredScar <thewolfbadger@gmail.com>
2026-04-20 14:47:07 -0400

    Implement encryption for collections and collection lists in the CollectionsClient. Add CompositeEncryptable trait implementation for CollectionView. Update tests to verify encryption and decryption roundtrips.

--- crates/bitwarden-vault/src/collection_client.rs
+++ crates/bitwarden-vault/src/collection_client.rs
@@ -12,7 +12,7 @@ use tsify::Tsify;
 #[cfg(feature = "wasm")]
 use wasm_bindgen::prelude::wasm_bindgen;
 
-use crate::DecryptError;
+use crate::{DecryptError, EncryptError};
 
 #[allow(missing_docs)]
 #[cfg_attr(feature = "wasm", wasm_bindgen)]
@@ -23,6 +23,23 @@ pub struct CollectionsClient {
 
 #[cfg_attr(feature = "wasm", wasm_bindgen)]
 impl CollectionsClient {
+    #[allow(missing_docs)]
+    pub fn encrypt(&self, collection_view: CollectionView) -> Result<Collection, EncryptError> {
+        let key_store = self.client.internal.get_key_store();
+        let collection = key_store.encrypt(collection_view)?;
+        Ok(collection)
+    }
+
+    #[allow(missing_docs)]
+    pub fn encrypt_list(
+        &self,
+        collection_views: Vec<CollectionView>,
+    ) -> Result<Vec<Collection>, EncryptError> {
+        let key_store = self.client.internal.get_key_store();
+        let collections = key_store.encrypt_list(&collection_views)?;
+        Ok(collections)
+    }
+
     #[allow(missing_docs)]
     pub fn decrypt(&self, collection: Collection) -> Result<CollectionView, DecryptError> {
         let key_store = self.client.internal.get_key_store();
@@ -132,11 +149,8 @@ mod tests {
     use super::*;
     use crate::VaultClientExt;
 
-    #[tokio::test]
-    async fn test_decrypt_list() {
-        let client = Client::init_test_account(test_bitwarden_com_account()).await;
-
-        let dec = client.vault().collections().decrypt_list(vec![Collection {
+    fn test_collection() -> Collection {
+        Collection {
             id: Some("66c5ca57-0868-4c7e-902f-b181009709c0".parse().unwrap()),
             organization_id: "1bc9ac1e-f5aa-45f2-94bf-b181009709b8".parse().unwrap(),
             name: "2.EI9Km5BfrIqBa1W+WCccfA==|laWxNnx+9H3MZww4zm7cBSLisjpi81zreaQntRhegVI=|x42+qKFf5ga6DIL0OW5pxCdLrC/gm8CXJvf3UASGteI=".parse().unwrap(),
@@ -146,7 +160,18 @@ mod tests {
             manage: false,
             default_user_collection_email: None,
             r#type: CollectionType::SharedCollection,
-        }]).unwrap();
+        }
+    }
+
+    #[tokio::test]
+    async fn test_decrypt_list() {
+        let client = Client::init_test_account(test_bitwarden_com_account()).await;
+
+        let dec = client
+            .vault()
+            .collections()
+            .decrypt_list(vec![test_collection()])
+            .unwrap();
 
         assert_eq!(dec[0].name, "Default collection");
     }
@@ -155,18 +180,67 @@ mod tests {
     async fn test_decrypt() {
         let client = Client::init_test_account(test_bitwarden_com_account()).await;
 
-        let dec = client.vault().collections().decrypt(Collection {
-            id: Some("66c5ca57-0868-4c7e-902f-b181009709c0".parse().unwrap()),
-            organization_id: "1bc9ac1e-f5aa-45f2-94bf-b181009709b8".parse().unwrap(),
-            name: "2.EI9Km5BfrIqBa1W+WCccfA==|laWxNnx+9H3MZww4zm7cBSLisjpi81zreaQntRhegVI=|x42+qKFf5ga6DIL0OW5pxCdLrC/gm8CXJvf3UASGteI=".parse().unwrap(),
-            external_id: None,
-            hide_passwords: false,
-            read_only: false,
-            manage: false,
-            default_user_collection_email: None,
-            r#type: CollectionType::SharedCollection,
-        }).unwrap();
+        let dec = client
+            .vault()
+            .collections()
+            .decrypt(test_collection())
+            .unwrap();
 
         assert_eq!(dec.name, "Default collection");
+    }
+
+    #[tokio::test]
+    async fn test_encrypt_decrypt_roundtrip() {
+        let client = Client::init_test_account(test_bitwarden_com_account()).await;
+
+        let view = client
+            .vault()
+            .collections()
+            .decrypt(test_collection())
+            .unwrap();
+
+        assert_eq!(view.name, "Default collection");
+
+        // Re-encrypt the decrypted view, then decrypt again
+        let expected_id = view.id;
+        let expected_org_id = view.organization_id;
+        let re_encrypted = client.vault().collections().encrypt(view).unwrap();
+        let re_decrypted = client.vault().collections().decrypt(re_encrypted).unwrap();
+
+        assert_eq!(re_decrypted.name, "Default collection");
+        assert_eq!(re_decrypted.id, expected_id);
+        assert_eq!(re_decrypted.organization_id, expected_org_id);
+    }
+
+    #[tokio::test]
+    async fn test_encrypt_list_decrypt_list_roundtrip() {
+        let client = Client::init_test_account(test_bitwarden_com_account()).await;
+
+        let views = client
+            .vault()
+            .collections()
+            .decrypt_list(vec![test_collection()])
+            .unwrap();
+
+        assert_eq!(views.len(), 1);
+        assert_eq!(views[0].name, "Default collection");
+
+        let expected_id = views[0].id;
+        let expected_org_id = views[0].organization_id;
+
+        let re_encrypted = client.vault().collections().encrypt_list(views).unwrap();
+
+        assert_eq!(re_encrypted.len(), 1);
+
+        let re_decrypted = client
+            .vault()
+            .collections()
+            .decrypt_list(re_encrypted)
+            .unwrap();
+
+        assert_eq!(re_decrypted.len(), 1);
+        assert_eq!(re_decrypted[0].name, "Default collection");
+        assert_eq!(re_decrypted[0].id, expected_id);
+        assert_eq!(re_decrypted[0].organization_id, expected_org_id);
     }
 }
