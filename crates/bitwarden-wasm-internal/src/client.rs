2b2fb4363cbdfc0be084c61ae44c89ec321e22b4
Bernd Schoolmann <mail@quexten.com>
2026-04-27 20:02:05 +0900

    Fix build

--- crates/bitwarden-wasm-internal/src/client.rs
+++ crates/bitwarden-wasm-internal/src/client.rs
@@ -1,7 +1,7 @@
 extern crate console_error_panic_hook;
 use std::{fmt::Display, sync::Arc};
 
-use bitwarden_core::ClientSettings;
+use bitwarden_core::{ClientSettings, key_management::state_bridge::StateBridgeClient};
 use bitwarden_error::bitwarden_error;
 use bitwarden_pm::{PasswordManagerClient as InnerPasswordManagerClient, clients::*};
 use bitwarden_user_crypto_management::{UserCryptoManagementClient, UserCryptoManagementClientExt};
