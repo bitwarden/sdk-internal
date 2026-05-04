507367d15436b8d9ed7521602f85c34fba97f38c
Bernd Schoolmann <bernd@schoolmann.dev>
2026-05-04 18:10:07 +0900

    Cleanup

--- crates/bitwarden-wasm-internal/src/lib.rs
+++ crates/bitwarden-wasm-internal/src/lib.rs
@@ -10,10 +10,6 @@ mod ssh;
 
 pub use bitwarden_ipc::wasm::*;
 pub use bitwarden_server_communication_config::wasm::*;
-// In order to fix an unused dependency error
-#[cfg(not(target_arch = "wasm32"))]
-pub use bitwarden_shared_unlock;
-#[cfg(target_arch = "wasm32")]
 pub use bitwarden_shared_unlock::wasm::*;
 pub use client::PasswordManagerClient;
 pub use flight_recorder::FlightRecorderClient;
