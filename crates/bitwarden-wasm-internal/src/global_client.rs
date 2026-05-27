c78dbdca287b464034f1dd0fdf3d2a6228b8d621
Andreas Coroiu <andreas.coroiu@gmail.com>
2026-05-27 13:11:59 +0200

    fix: lint

--- crates/bitwarden-wasm-internal/src/global_client.rs
+++ crates/bitwarden-wasm-internal/src/global_client.rs
@@ -4,12 +4,15 @@ use wasm_bindgen::prelude::*;
 
 /// The main entry point for Global Bitwarden SDK operations in WebAssembly environments
 #[wasm_bindgen]
-pub struct GlobalPasswordManagerClient(pub(crate) InnerGlobalPasswordManagerClient);
+pub struct GlobalPasswordManagerClient(
+    #[allow(dead_code)] pub(crate) InnerGlobalPasswordManagerClient,
+);
 
 #[wasm_bindgen]
 impl GlobalPasswordManagerClient {
     /// Initialize a new instance of the SDK client
     #[wasm_bindgen(constructor)]
+    #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
         Self(InnerGlobalPasswordManagerClient(GlobalClient::new()))
     }
