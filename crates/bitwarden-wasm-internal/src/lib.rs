eae1e047789bc8b3d56f521100fbe3250b362373
Hinton <hinton@users.noreply.github.com>
2026-07-10 13:45:03 +0200

    Drop pam wiring to wasm

--- crates/bitwarden-wasm-internal/src/lib.rs
+++ crates/bitwarden-wasm-internal/src/lib.rs
@@ -10,8 +10,6 @@ mod ssh;
 
 pub use bitwarden_ipc::wasm::*;
 pub use bitwarden_organization_invite_link::*;
-#[cfg(feature = "bitwarden-license")]
-pub use bitwarden_pam::*;
 pub use bitwarden_server_communication_config::wasm::*;
 pub use bitwarden_shared_unlock::wasm::*;
 pub use client::PasswordManagerClient;
