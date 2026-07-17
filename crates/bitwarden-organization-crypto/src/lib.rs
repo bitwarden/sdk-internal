779e13a916606b45c37f0b5e8a7a112aa2b79c6a
Bernd Schoolmann <bernd@schoolmann.dev>
2026-07-17 21:10:36 +0900

    fixup! chore(invite-link): cleanup old invite logic

--- crates/bitwarden-organization-crypto/src/lib.rs
+++ crates/bitwarden-organization-crypto/src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 
-pub use invite::{Invite, InviteSecret, InviteKeyBundleError};
+pub use invite::{Invite, InviteKeyBundleError, InviteSecret};
 mod invite;
 
 #[cfg(feature = "wasm")]
