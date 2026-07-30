0f80aec575785faf75b6c39cc88c5154b1ab2a9a
Bernd Schoolmann <bernd@schoolmann.dev>
2026-07-30 23:45:32 +0900

    fixup! feat(crypto): add user-key id tracking

--- crates/bitwarden-pm/tests/km_export_check.rs
+++
@@ -1,7 +0,0 @@
-#[test]
-fn km_sync_user_decryption_is_nameable_downstream() {
-    let _ = bitwarden_km_sync_handler::KmSyncData {
-        user_decryption: Some(bitwarden_km_sync_handler::KmSyncUserDecryption::default()),
-        ..Default::default()
-    };
-}
