ca039e0af4b602e8d89187d537e954dddbcfeab5
Bernd Schoolmann <bernd@schoolmann.dev>
2026-08-03 21:39:05 +0900

    chore: remove unused invite logic (#1348)
    Removes now unused old invite formats.

--- crates/bitwarden-organization-crypto/src/lib.rs
+++ crates/bitwarden-organization-crypto/src/lib.rs
@@ -1,7 +1,5 @@
 #![doc = include_str!("../README.md")]
 
-mod invite_key_bundle;
-pub use invite_key_bundle::{Invite, InviteBundle, InviteKeyBundleError, InviteKeyData};
 pub mod invite;
 
 #[cfg(feature = "wasm")]
